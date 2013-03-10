9f42e8b4933d91f6513edc88962d53bbce4928d3
James Kuszmaul <jabukuszmaul@gmail.com>
2013-03-10 16:44:47 -0700

    Added a tests for dealing with bad output/position. Currently, they fail.

--- frc971/control_loops/drivetrain/drivetrain_lib_test.cc
+++ frc971/control_loops/drivetrain/drivetrain_lib_test.cc
@@ -144,6 +144,41 @@ TEST_F(DrivetrainTest, ConvergesCorrectly) {
   VerifyNearGoal();
 }
 
+// Tests that it survives disabling.
+TEST_F(DrivetrainTest, SurvivesZeroing) {
+  my_drivetrain_loop_.goal.MakeWithBuilder().control_loop_driving(true)
+      .left_goal(-1.0)
+      .right_goal(1.0).Send();
+  for (int i = 0; i < 500; ++i) {
+    if (i > 20 && i < 200) {
+      SendDSPacket(false);
+    } else {
+      SendDSPacket(true);
+    }
+    drivetrain_motor_plant_.SendPositionMessage();
+    drivetrain_motor_.Iterate();
+    drivetrain_motor_plant_.Simulate();
+  }
+  VerifyNearGoal();
+}
+
+// Tests surviving bad positions.
+TEST_F(DrivetrainTest, SurvivesBadPosition) {
+  my_drivetrain_loop_.goal.MakeWithBuilder().control_loop_driving(true)
+      .left_goal(-1.0)
+      .right_goal(1.0).Send();
+  for (int i = 0; i < 500; ++i) {
+    if (i > 20 && i < 200) {
+    } else {
+      drivetrain_motor_plant_.SendPositionMessage();
+    }
+    drivetrain_motor_.Iterate();
+    drivetrain_motor_plant_.Simulate();
+    SendDSPacket(true);
+  }
+  VerifyNearGoal();
+}
+
 }  // namespace testing
 }  // namespace control_loops
 }  // namespace frc971
