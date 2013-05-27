f6adc21c553726b826cd51661af2b81aa9b52ebe
Brian Silverman <brians>
2013-05-26 17:05:01 -0700

    fixed the same off-by-1 in a second place

--- aos/externals/WPILib/WPILib/NetworkRobot/NetworkRobotValues.h
+++ aos/externals/WPILib/WPILib/NetworkRobot/NetworkRobotValues.h
@@ -143,7 +143,7 @@ class Write {
   }
 
   // Returns whether or not we ran over the end.
-  bool overrun() const { return current_index_ >= data_size_; }
+  bool overrun() const { return current_index_ > data_size_; }
 
  private:
   // Where we are reading or writing from. Not owned by this object.
