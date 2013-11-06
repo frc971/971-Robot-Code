8c0eed88fd613ffdb5568dd18e857e58fea4e92e
James Kuszmaul <james@hp-debian>
2013-11-05 20:28:05 -0800

    Made Velocity Shooter loop such that it should run.
    Currently, all it builds and passes all the tests. I need to go in and check to see if there are any more tests I should add.

--- bot3/control_loops/shooter/shooter_motor_plant.h
+++ bot3/control_loops/shooter/shooter_motor_plant.h
@@ -1,9 +1,9 @@
-#ifndef FRC971_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
-#define FRC971_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
+#ifndef BOT3_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
+#define BOT3_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
 
 #include "frc971/control_loops/state_feedback_loop.h"
 
-namespace frc971 {
+namespace bot3 {
 namespace control_loops {
 
 StateFeedbackPlantCoefficients<1, 1, 1> MakeShooterPlantCoefficients();
@@ -15,6 +15,6 @@ StateFeedbackPlant<1, 1, 1> MakeShooterPlant();
 StateFeedbackLoop<1, 1, 1> MakeShooterLoop();
 
 }  // namespace control_loops
-}  // namespace frc971
+}  // namespace bot3
 
-#endif  // FRC971_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
+#endif  // BOT3_CONTROL_LOOPS_SHOOTER_SHOOTER_MOTOR_PLANT_H_
