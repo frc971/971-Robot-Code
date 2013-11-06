adab8f9ac146cb2d89c4371d2f455055c393c410
James Kuszmaul <james@hp-debian>
2013-11-06 08:42:34 -0800

    Moved bot3-specific python stuff in with bot3.

--- bot3/control_loops/python/control_loop.py
+++ bot3/control_loops/python/control_loop.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('../../frc971/control_loops/python')
 import controls
 import numpy
 
@@ -17,7 +19,7 @@ class ControlLoopWriter(object):
     if namespaces:
       self._namespaces = namespaces
     else:
-      self._namespaces = ['frc971', 'control_loops']
+      self._namespaces = ['bot3', 'control_loops']
 
     self._namespace_start = '\n'.join(
         ['namespace %s {' % name for name in self._namespaces])
@@ -26,7 +28,7 @@ class ControlLoopWriter(object):
         ['}  // namespace %s' % name for name in reversed(self._namespaces)])
 
   def _HeaderGuard(self, header_file):
-    return ('FRC971_CONTROL_LOOPS_' +
+    return ('BOT3_CONTROL_LOOPS_' +
             header_file.upper().replace('.', '_').replace('/', '_') +
             '_')
 
@@ -89,7 +91,7 @@ class ControlLoopWriter(object):
   def WriteCC(self, header_file_name, cc_file):
     """Writes the cc file to the file named cc_file."""
     with open(cc_file, 'w') as fd:
-      fd.write('#include \"frc971/control_loops/%s\"\n' % header_file_name)
+      fd.write('#include \"bot3/control_loops/%s\"\n' % header_file_name)
       fd.write('\n')
       fd.write('#include <vector>\n')
       fd.write('\n')
