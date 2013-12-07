44311d612ed346b711862dd3197e677c8ba5022c
Brian Silverman <brians>
2013-12-06 22:03:29 -0800

    got the startup setup for both the bootloader and main code figured out

--- bbb_cape/src/cape/bootloader.c
+++ bbb_cape/src/cape/bootloader.c
@@ -1,2 +1,19 @@
+#include <stdint.h>
+
+#include "cape/bootloader_handoff.h"
+
+// Sets everything up and then jumps to the main code.
+static void jump_to_main(void) __attribute__((noreturn));
+static void jump_to_main(void) {
+  // 0x20008000
+  __asm__ volatile(
+      "mov sp, %[stack]\n\t"
+      "bx %[reset]" : :
+      [stack]"r"(RAM_START + RAM_SIZE), [reset]"r"(MAIN_FLASH_START | 1)
+      : "memory");
+  __builtin_unreachable();
+}
+
 void _start(void) {
+  jump_to_main();
 }
--- bbb_cape/src/cape/main.c
+++ bbb_cape/src/cape/main.c
@@ -1,2 +1,12 @@
+#include <STM32F2XX.h>
+
+#include "cape/bootloader_handoff.h"
+
+// The startup asm code defines this to the start of our exception vector table.
+extern uint32_t _vectors;
+
 void _start(void) {
+  // Change the vector table offset to use our vector table instead of the
+  // bootloader's.
+  SCB->VTOR = (uint32_t)&_vectors;
 }
