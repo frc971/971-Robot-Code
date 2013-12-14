e3acf711908f1396e419f9394cc15a31c12fc604
Brian Silverman <brians>
2013-12-14 10:48:10 -0800

    played around with initialization code

--- bbb_cape/src/cape/main.c
+++ bbb_cape/src/cape/main.c
@@ -9,6 +9,18 @@ void _start(void) {
   // Change the vector table offset to use our vector table instead of the
   // bootloader's.
   SCB->VTOR = (uint32_t)&_vectors;
+  // Data Memory Barrier to make sure it gets the updated vector table.
+  __asm__ __volatile__("dmb");
 
   fill_packet_start();
+
+  // Make it go right to sleep after handling all exceptions. This actually
+  // decreses ISR latency a little bit because it doesn't have to stack the
+  // registers for the first one.
+  SCB->SCR |= SCB_SCR_SLEEPONEXIT_Msk;
+  // This seems like the perfect place to use WFI, but Brian on 2013-12-13
+  // couldn't find anything verifying that WFI doesn't increase the latency for
+  // the first interrupt handled, and we should never actually get here anyways,
+  // so it doesn't matter.
+  while (1) {}
 }
