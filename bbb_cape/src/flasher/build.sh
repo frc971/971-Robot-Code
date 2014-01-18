3c7c86324d2c64f6e914634fe0d1a5de7bb18470
Brian Silverman <brians>
2014-01-17 22:19:19 -0800

    fixed the build script after merging Daniel's stuff and updated upstream

--- bbb_cape/src/flasher/build.sh
+++ bbb_cape/src/flasher/build.sh
@@ -5,9 +5,6 @@ set -e
 cd $(dirname $0)
 
 [[ -d stm32flash ]] || ( git clone https://git.gitorious.org/stm32flash/stm32flash.git stm32flash &&
-	cd stm32flash && git checkout 5b0e391c539e906df7b97f0b457875d90883ea8e && patch -p1 < ../0001-fixed-the-page-by-page-erase-logic.patch )
+	cd stm32flash && git checkout 8399fbe1baf2b7d097746786458021d92895d71b )
 
-# TODO(brians): This breaks the build of the main code. Figure out something
-# better for this stuff.
-
-../../../aos/build/build.sh atom flasher.gyp no "$@"
+../../../aos/build/build.sh linux flasher.gyp no flasher "$@"
