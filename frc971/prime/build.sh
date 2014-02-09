023dc5116c1168cef844c47e252c55a03b57b16a
Brian Silverman <brians>
2014-02-09 14:50:33 -0800

    added options for only building some of the code

--- frc971/prime/build.sh
+++ frc971/prime/build.sh
@@ -2,5 +2,16 @@
 
 cd $(dirname $0)
 
-../../aos/build/build.sh linux prime.gyp no prime "$@"
-../../aos/build/build.sh linux-amd64 prime.gyp no prime-amd64 "$@"
+if [[ "$1" == "amd64" ]]; then
+  NO_TARGET=1
+  shift 1
+elif [[ "$1" == "arm" ]]; then
+  NO_AMD64=1
+  shift 1
+fi
+
+[[ "$1" == "tests" ]] && NO_TARGET=1
+
+[[ ${NO_TARGET} ]] || ../../aos/build/build.sh linux prime.gyp no prime "$@"
+[[ ${NO_AMD64} ]] || \
+    ../../aos/build/build.sh linux-amd64 prime.gyp no prime-amd64 "$@"
