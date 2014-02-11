f884efc4baf888f330e81c24e392cd2a574cb16a
Austin Schuh <austin@peloton-tech.com>
2014-02-10 21:19:03 -0800

    Doesn't build ARM if AMD64 fails to compile.

--- frc971/prime/build.sh
+++ frc971/prime/build.sh
@@ -2,5 +2,5 @@
 
 cd $(dirname $0)
 
+../../aos/build/build.sh linux-amd64 prime.gyp no prime-amd64 "$@" || exit 1
 ../../aos/build/build.sh linux prime.gyp no prime "$@"
-../../aos/build/build.sh linux-amd64 prime.gyp no prime-amd64 "$@"
