acee44a308b8b59453e9c8d49d5c9a9b5bf9907b
Brian Silverman <brians>
2014-02-12 14:07:37 -0800

    added more helpful messages to the build scripts

--- frc971/prime/build.sh
+++ frc971/prime/build.sh
@@ -14,20 +14,20 @@ fi
 [[ "$1" == "deploy" ]] && NO_AMD64=1
 
 if [[ ! ${NO_TARGET} ]]; then
-	echo 'Building code for target...' 2>&1
+	echo 'Building code for target...' 1>&2
 	../../aos/build/build.sh linux prime.gyp no prime "$@"
     if [[ $? -ne 0 ]]; then
-		echo 'Building code for target failed!' 2>&1
+		echo 'Building code for target failed!' 1>&2
 		exit 1
 	fi
-	echo 'Building code for target succeeded.' 2>&1
+	echo 'Building code for target succeeded.' 1>&2
 fi
 if [[ ! ${NO_AMD64} ]]; then
-	echo 'Building code for amd64...' 2>&1
-	../../aos/build/build.sh linux-amd64 prime.gyp no prime-amd64 "$@"
+	echo 'Building code for amd64...' 1>&2
+	../../aos/build/build.sh linux-amd64 prime.gyp yes prime-amd64 "$@"
     if [[ $? -ne 0 ]]; then
-		echo 'Building code for amd64 failed!' 2>&1
+		echo 'Building code for amd64 failed!' 1>&2
 		exit 1
 	fi
-	echo 'Building code for amd64 succeeded.' 2>&1
+	echo 'Building code for amd64 succeeded.' 1>&2
 fi
