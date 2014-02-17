c47182f6b46e8df4d8d68688b9d5b0adb7a37403
Brian Silverman <brians>
2014-02-16 21:43:36 -0800

    not quite sure what I was thinking when I wrote it the first time...

--- bbb_cape/src/cape/robot_comp.c
+++ bbb_cape/src/cape/robot_comp.c
@@ -17,7 +17,8 @@ void TIM1_TRG_COM_TIM11_IRQHandler(void) {
 }
 
 void robot_init(void) {
-  gpio_setup_alt(GPIOB, 11, 3);
+  // Digital input 6.
+  gpio_setup_alt(GPIOB, 9, 3);
   RCC->APB2ENR |= RCC_APB2ENR_TIM11EN;
   TIM11->CR1 = TIM_CR1_URS;
   TIM11->SMCR = 5 << 4 /* TI1 */ |
