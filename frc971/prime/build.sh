f81e9a50730690831a6281678fbe77e6c5ae61b3
Brian Silverman <brians>
2014-04-21 19:02:51 -0700

    cleaning up build scripts in preparation for adding a clang build

--- frc971/prime/build.sh
+++ frc971/prime/build.sh
@@ -2,32 +2,4 @@
 
 cd $(dirname $0)
 
-if [[ "$1" == "amd64" ]]; then
-  NO_TARGET=1
-  shift 1
-elif [[ "$1" == "arm" ]]; then
-  NO_AMD64=1
-  shift 1
-fi
-
-[[ "$1" == "tests" ]] && NO_TARGET=1
-[[ "$1" == "deploy" ]] && NO_AMD64=1
-
-if [[ ! ${NO_TARGET} ]]; then
-	echo 'Building code for target...' 1>&2
-	../../aos/build/build.sh linux prime.gyp no prime "$@"
-    if [[ $? -ne 0 ]]; then
-		echo 'Building code for target failed!' 1>&2
-		exit 1
-	fi
-	echo 'Building code for target succeeded.' 1>&2
-fi
-if [[ ! ${NO_AMD64} ]]; then
-	echo 'Building code for amd64...' 1>&2
-	../../aos/build/build.sh linux-amd64 prime.gyp yes prime-amd64 "$@"
-    if [[ $? -ne 0 ]]; then
-		echo 'Building code for amd64 failed!' 1>&2
-		exit 1
-	fi
-	echo 'Building code for amd64 succeeded.' 1>&2
-fi
+../../aos/build/prime-build.sh "$@"
