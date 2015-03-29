17bb36d0af5347eb9309c86a49cac2ae880d55dc
Brian Silverman <brians>
2015-03-29 16:50:06 -0400

    actually restart the code on the roborio when it crashes
    Change-Id: I647e705dcba5294068c402947cd2e0871fa49094

--- aos/linux_code/starter/starter.sh
+++ aos/linux_code/starter/starter.sh
@@ -3,5 +3,7 @@
 # NI already has a core pattern, so we probably shouldn't change it.
 #echo '/home/driver/tmp/robot_logs/%e-%s-%p-%t.coredump' > /proc/sys/kernel/core_pattern
 
-export PATH=$PATH:/home/admin/robot_code
-exec starter_exe /home/admin/robot_code/start_list.txt
+while true; do
+	export PATH=$PATH:/home/admin/robot_code
+	starter_exe /home/admin/robot_code/start_list.txt
+done
