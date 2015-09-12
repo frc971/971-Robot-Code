166aea6b0a14b785b67be3943d9faa48ab18263a
Austin Schuh <austin@peloton-tech.com>
2015-09-11 23:47:11 -0700

    Modified control loops to run in bot3.
    Change-Id: I5d9d461c8a10dc1c54fee69273809f76328df655

--- bot3/control_loops/update_drivetrain.sh
+++ bot3/control_loops/update_drivetrain.sh
@@ -4,6 +4,8 @@
 
 cd $(dirname $0)
 
+export PYTHONPATH=../../frc971/control_loops/python
+
 ./python/drivetrain.py drivetrain/drivetrain_dog_motor_plant.h \
     drivetrain/drivetrain_dog_motor_plant.cc \
     drivetrain/drivetrain_clutch_motor_plant.h \
--- bot3/control_loops/update_polydrivetrain.sh
+++ bot3/control_loops/update_polydrivetrain.sh
@@ -4,6 +4,8 @@
 
 cd $(dirname $0)
 
+export PYTHONPATH=../../frc971/control_loops/python
+
 ./python/polydrivetrain.py drivetrain/polydrivetrain_dog_motor_plant.h \
     drivetrain/polydrivetrain_dog_motor_plant.cc \
     drivetrain/polydrivetrain_clutch_motor_plant.h \
