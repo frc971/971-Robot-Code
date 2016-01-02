4be7ffe0fc1d3242814577ea9c33dfe4c6eee3ce
Brian Silverman <brians>
2016-01-02 14:16:06 -0800

    Update wpilib_robot_base to work with new WPILib too
    Change-Id: I911cc1eb304c8e33ddd9a60c0ffffe1ef577bcd0

--- frc971/wpilib/wpilib_robot_base.h
+++ frc971/wpilib/wpilib_robot_base.h
@@ -1,7 +1,11 @@
 #ifndef FRC971_WPILIB_NEWROBOTBASE_H_
 #define FRC971_WPILIB_NEWROBOTBASE_H_
 
+#ifdef WPILIB2015
 #include "RobotBase.h"
+#else
+#include "SampleRobot.h"
+#endif
 
 namespace frc971 {
 namespace wpilib {
@@ -15,16 +19,23 @@ public:
   START_ROBOT_CLASS(::frc971::wpilib::WPILibAdapterRobot<_ClassName_>)
 
 template <typename T>
+#ifdef WPILIB2015
 class WPILibAdapterRobot : public RobotBase {
+#else
+class WPILibAdapterRobot : public SampleRobot {
+#endif
  public:
+#ifdef WPILIB2015
   void StartCompetition() override { robot_.Run(); }
+#else
+  void RobotMain() override { robot_.Run(); }
+#endif
 
  private:
   T robot_;
 };
 
-}
-}
+}  // namespace wpilib
+}  // namespace frc971
 
-#endif // FRC971_WPILIB_NEWROBOTBASE_H_
-
+#endif  // FRC971_WPILIB_NEWROBOTBASE_H_
