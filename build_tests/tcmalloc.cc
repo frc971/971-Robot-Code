2dfc9cffb8d756a1293702bffbfa4d6e8311fc7b
Brian Silverman <bsilver16384@gmail.com>
2017-02-19 14:20:40 -0800

    Make the compiler not optimize my test out
    Austin discovered it was broken with -c opt.
    
    Change-Id: If2008f98b8079c9c40f8050400e3a250414d909f

--- build_tests/tcmalloc.cc
+++ build_tests/tcmalloc.cc
@@ -1,6 +1,8 @@
 #include <stdlib.h>
+#include <unistd.h>
 
 int main() {
-  void *big_chunk = malloc(500000);
+  void *big_chunk = malloc(500000 + read(-1, nullptr, 0));
+  if (write(-1, big_chunk, 0) != -1) return 1;
   free(big_chunk);
 }
