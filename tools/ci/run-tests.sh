a826be1e2c00688251c0803e375c3f4a66fea4d4
Austin Schuh <austin@peloton-tech.com>
2018-01-03 21:47:33 -0800

    Push --jobs to ~/.bazelrc so we can use all of iron.
    Change-Id: Ib79f9c3eb1335331e4ae72e2ceb883403361b2d7

--- tools/ci/run-tests.sh
+++ tools/ci/run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 
-bazel --batch test -c opt --curses=no --color=no --jobs=1 //...
-bazel --batch build -c opt --curses=no --color=no --jobs=1 //... --cpu=roborio
-bazel --batch build -c opt --curses=no --color=no --jobs=1 //motors/... --cpu=cortex-m4f
+bazel --batch test -c opt --curses=no --color=no //...
+bazel --batch build -c opt --curses=no --color=no //... --cpu=roborio
+bazel --batch build -c opt --curses=no --color=no //motors/... --cpu=cortex-m4f
