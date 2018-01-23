92ebcbb07bf83d2265f47bfd45d4f7586304a96c
Austin Schuh <austin.linux@gmail.com>
2018-01-23 11:17:08 -0800

    Remove auto usage from RK
    Change-Id: Ibf3b374ffd0ac8fc94a871797e198557cf3615e3

--- frc971/control_loops/runge_kutta.h
+++ frc971/control_loops/runge_kutta.h
@@ -12,10 +12,10 @@ namespace control_loops {
 template <typename F, typename T>
 T RungeKutta(const F &fn, T X, double dt) {
   const double half_dt = dt * 0.5;
-  auto k1 = fn(X);
-  auto k2 = fn(X + half_dt * k1);
-  auto k3 = fn(X + half_dt * k2);
-  auto k4 = fn(X + dt * k3);
+  T k1 = fn(X);
+  T k2 = fn(X + half_dt * k1);
+  T k3 = fn(X + half_dt * k2);
+  T k4 = fn(X + dt * k3);
   return X + dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4);
 }
 
