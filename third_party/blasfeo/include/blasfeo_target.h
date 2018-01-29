d94fb23a8e5b4001842d2c93f8601fbce366c4b8
Austin Schuh <austin.linux@gmail.com>
2018-01-28 17:09:21 -0800

    Added BUILD rules for blasfeo and hpipm
    These are needed for the controls toolbox ct.
    
    Change-Id: I50608f01abfdf24ad52f1dda5a4c81fd079afb16

---
+++ third_party/blasfeo/include/blasfeo_target.h
@@ -0,0 +1,9 @@
+#ifndef TARGET_X64_INTEL_HASWELL
+#define TARGET_X64_INTEL_HASWELL
+#endif
+#ifndef LA_HIGH_PERFORMANCE
+#define LA_HIGH_PERFORMANCE
+#endif
+#ifndef EXT_DEP
+#define EXT_DEP
+#endif
