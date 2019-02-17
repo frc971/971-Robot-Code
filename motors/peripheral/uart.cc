4f958799d2c820fffb43ae4918a31347612e6859
Brian Silverman <bsilver16384@gmail.com>
2019-02-16 18:20:04 -0800

    Modify the uart_buffer code to support reading
    For reads, we push one character at a time and then read multiple ones.
    We want to do this for the camera board.
    
    Change-Id: I8a32dcaf0aa822a5642f388c58e02e17564203f3

--- motors/peripheral/uart.cc
+++ motors/peripheral/uart.cc
@@ -39,18 +39,24 @@ void Uart::Initialize(int baud_rate) {
   // M_UART_RIE /* Enable RX interrupt or DMA */
   // Also set in C5: M_UART_TDMAS /* Do DMA for TX */ |
   // M_UART_RDMAS /* Do DMA for RX */
-  c2_value_ = M_UART_TE;
+  c2_value_ = 0;
   module_->C2 = c2_value_;
   module_->PFIFO =
       M_UART_TXFE /* Enable TX FIFO */ | M_UART_RXFE /* Enable RX FIFO */;
   module_->CFIFO =
       M_UART_TXFLUSH /* Flush TX FIFO */ | M_UART_RXFLUSH /* Flush RX FIFO */;
+  c2_value_ = M_UART_TE | M_UART_RE;
+  module_->C2 = c2_value_;
   // TODO(Brian): Adjust for DMA?
   module_->TWFIFO = tx_fifo_size_ - 1;
-  module_->RWFIFO = rx_fifo_size_ - 1;
+  module_->RWFIFO = 1;
 }
 
 void Uart::DoWrite(gsl::span<const char> data) {
+  // In theory, we could be more efficient about this by writing the number of
+  // bytes we know there's space for and only calling SpaceAvailable() (or
+  // otherwise reading S1) before the final one. In practice, the FIFOs are so
+  // short on this part it probably won't help anything.
   for (int i = 0; i < data.size(); ++i) {
     while (!SpaceAvailable()) {
     }
@@ -58,38 +64,91 @@ void Uart::DoWrite(gsl::span<const char> data) {
   }
 }
 
-Uart::~Uart() { DisableTransmitInterrupt(); }
+aos::SizedArray<char, 4> Uart::DoRead() {
+  // In theory, we could be more efficient about this by reading the number of
+  // bytes we know to be accessible and only calling DataAvailable() (or
+  // otherwise reading S1) before the final one. In practice, the FIFOs are so
+  // short on this part it probably won't help anything.
+  aos::SizedArray<char, 4> result;
+  while (DataAvailable() && !result.full()) {
+    result.push_back(ReadCharacter());
+  }
+  return result;
+}
+
+Uart::~Uart() {
+  DoDisableTransmitInterrupt();
+  DoDisableReceiveInterrupt();
+}
+
+InterruptBufferedUart::~InterruptBufferedUart() {
+  uart_.DisableReceiveInterrupt(DisableInterrupts());
+}
 
 void InterruptBufferedUart::Initialize(int baud_rate) {
   uart_.Initialize(baud_rate);
+  {
+    DisableInterrupts disable_interrupts;
+    uart_.EnableReceiveInterrupt(disable_interrupts);
+  }
 }
 
 void InterruptBufferedUart::Write(gsl::span<const char> data) {
   DisableInterrupts disable_interrupts;
-  uart_.EnableTransmitInterrupt();
-  static_assert(buffer_.size() >= 8,
-                "Need enough space to not turn the interrupt off each time");
+  uart_.EnableTransmitInterrupt(disable_interrupts);
   while (!data.empty()) {
-    const int bytes_written = buffer_.PushSpan(data);
+    const int bytes_written = transmit_buffer_.PushSpan(data);
     data = data.subspan(bytes_written);
     WriteCharacters(data.empty(), disable_interrupts);
     ReenableInterrupts{&disable_interrupts};
   }
 }
 
-void InterruptBufferedUart::WriteCharacters(bool disable_empty,
-                                            const DisableInterrupts &) {
+gsl::span<char> InterruptBufferedUart::Read(gsl::span<char> buffer) {
+  size_t bytes_read = 0;
+  {
+    DisableInterrupts disable_interrupts;
+    const gsl::span<const char> read_data =
+        receive_buffer_.PopSpan(buffer.size());
+    std::copy(read_data.begin(), read_data.end(), buffer.begin());
+    bytes_read += read_data.size();
+  }
+  {
+    DisableInterrupts disable_interrupts;
+    const gsl::span<const char> read_data =
+        receive_buffer_.PopSpan(buffer.size() - bytes_read);
+    std::copy(read_data.begin(), read_data.end(),
+              buffer.subspan(bytes_read).begin());
+    bytes_read += read_data.size();
+  }
+  return buffer.subspan(0, bytes_read);
+}
+
+void InterruptBufferedUart::WriteCharacters(
+    bool disable_empty, const DisableInterrupts &disable_interrupts) {
   while (true) {
-    if (buffer_.empty()) {
+    if (transmit_buffer_.empty()) {
       if (disable_empty) {
-        uart_.DisableTransmitInterrupt();
+        uart_.DisableTransmitInterrupt(disable_interrupts);
       }
       return;
     }
     if (!uart_.SpaceAvailable()) {
       return;
     }
-    uart_.WriteCharacter(buffer_.PopSingle());
+    uart_.WriteCharacter(transmit_buffer_.PopSingle());
+  }
+}
+
+void InterruptBufferedUart::ReadCharacters(const DisableInterrupts &) {
+  while (true) {
+    if (receive_buffer_.full()) {
+      return;
+    }
+    if (!uart_.DataAvailable()) {
+      return;
+    }
+    receive_buffer_.PushSingle(uart_.ReadCharacter());
   }
 }
 
--- motors/peripheral/uart.h
+++ motors/peripheral/uart.h
@@ -1,6 +1,7 @@
 #ifndef MOTORS_PERIPHERAL_UART_H_
 #define MOTORS_PERIPHERAL_UART_H_
 
+#include "aos/containers/sized_array.h"
 #include "motors/core/kinetis.h"
 #include "motors/peripheral/uart_buffer.h"
 #include "motors/util.h"
@@ -25,22 +26,59 @@ class Uart {
     DoWrite(data);
   }
 
+  // Returns all the data which is currently available.
+  aos::SizedArray<char, 4> Read(const DisableInterrupts &) {
+    return DoRead();
+  }
+
   bool SpaceAvailable() const { return module_->S1 & M_UART_TDRE; }
   // Only call this if SpaceAvailable() has just returned true.
   void WriteCharacter(char c) { module_->D = c; }
 
-  void EnableTransmitInterrupt() {
+  bool DataAvailable() const { return module_->S1 & M_UART_RDRF; }
+  // Only call this if DataAvailable() has just returned true.
+  char ReadCharacter() { return module_->D; }
+
+  // TODO(Brian): These APIs for enabling/disabling interrupts aren't quite
+  // right. Redo them some time. Some issues:
+  //   * They get called during initialization/destruction time, which means
+  //     interrupts don't really need to be disabled because everything is
+  //     singlethreaded.
+  //   * Often, several C2 modifications are made in a single
+  //     interrupts-disabled section. These could be batched to reduce
+  //     peripheral writes. Sometimes, no modifications are made at all, in
+  //     which case there doesn't even need to be a single write.
+
+  void EnableTransmitInterrupt(const DisableInterrupts &) {
     c2_value_ |= M_UART_TIE;
     module_->C2 = c2_value_;
   }
 
-  void DisableTransmitInterrupt() {
-    c2_value_ &= ~M_UART_TIE;
+  void DisableTransmitInterrupt(const DisableInterrupts &) {
+    DoDisableTransmitInterrupt();
+  }
+
+  void EnableReceiveInterrupt(const DisableInterrupts &) {
+    c2_value_ |= M_UART_RIE;
     module_->C2 = c2_value_;
   }
 
+  void DisableReceiveInterrupt(const DisableInterrupts &) {
+    DoDisableReceiveInterrupt();
+  }
+
  private:
+  void DoDisableTransmitInterrupt() {
+    c2_value_ &= ~M_UART_TIE;
+    module_->C2 = c2_value_;
+  }
+  void DoDisableReceiveInterrupt() {
+    c2_value_ &= ~M_UART_RIE;
+    module_->C2 = c2_value_;
+  }
+
   void DoWrite(gsl::span<const char> data);
+  aos::SizedArray<char, 4> DoRead();
 
   KINETISK_UART_t *const module_;
   const int module_clock_frequency_;
@@ -51,27 +89,37 @@ class Uart {
 };
 
 // Interrupt-based buffered interface to a UART.
+// TODO(Brian): Move DisableInterrupts calls up to the caller of this.
 class InterruptBufferedUart {
  public:
   InterruptBufferedUart(KINETISK_UART_t *module, int module_clock_frequency)
       : uart_(module, module_clock_frequency) {}
+  ~InterruptBufferedUart();
 
   void Initialize(int baud_rate);
 
+  // Queues up the given data for immediate writing. Blocks only if the queue
+  // fills up before all of data is enqueued.
   void Write(gsl::span<const char> data);
+
+  // Reads currently available data.
+  // Returns all the data which is currently available (possibly none);
+  // buffer is where to store the result. The return value will be a subspan of
+  // this.
+  gsl::span<char> Read(gsl::span<char> buffer);
 
   // Should be called as the body of the interrupt handler.
   void HandleInterrupt(const DisableInterrupts &disable_interrupts) {
     WriteCharacters(true, disable_interrupts);
+    ReadCharacters(disable_interrupts);
   }
 
  private:
   void WriteCharacters(bool disable_empty, const DisableInterrupts &);
+  void ReadCharacters(const DisableInterrupts &);
 
   Uart uart_;
-  UartBuffer<1024> buffer_;
-
-  bool interrupt_enabled_ = false;
+  UartBuffer<1024> transmit_buffer_, receive_buffer_;
 };
 
 }  // namespace teensy
