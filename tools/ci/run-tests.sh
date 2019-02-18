ca26b523fce648e84fc03a26d9d4104107d9150e
Austin Schuh <austin.linux@gmail.com>
2019-02-17 17:25:14 -0800

    Turn stamping off for CI to save space.
    Change-Id: Ia24892ed80de080dee5558234086342827d23bd1

--- tools/ci/run-tests.sh
+++ tools/ci/run-tests.sh
@@ -6,7 +6,8 @@ TARGETS='//... @com_github_google_glog//... @com_google_ceres_solver//...'
 
 # Include --config=eigen to enable Eigen assertions so that we catch potential
 # bugs with Eigen.
-bazel test -c opt --config=eigen --curses=no --color=no ${TARGETS}
-bazel build -c opt --curses=no --color=no ${TARGETS} --cpu=roborio
-bazel build --curses=no --color=no ${TARGETS} --cpu=armhf-debian
-bazel build -c opt --curses=no --color=no //motors/... //y2019/jevois/... --cpu=cortex-m4f
+bazel test -c opt --stamp=no --config=eigen --curses=no --color=no ${TARGETS}
+bazel build -c opt --stamp=no --curses=no --color=no ${TARGETS} --cpu=roborio
+bazel build --stamp=no --curses=no --color=no ${TARGETS} --cpu=armhf-debian
+bazel build -c opt --stamp=no --curses=no --color=no \
+    //motors/... //y2019/jevois/... --cpu=cortex-m4f
