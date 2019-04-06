2d177bb4494cdef053223bc7ec8c389e4ebabf26
James Kuszmaul <jabukuszmaul@gmail.com>
2019-04-06 16:29:37 -0700

    Recalibrate practice bot elevator
    Change-Id: I11b60c480bb4bb6f772684cdf9b7bd09eda5fb41

--- y2019/constants.cc
+++ y2019/constants.cc
@@ -194,8 +194,8 @@ const Values *DoGetValuesForTeam(uint16_t team) {
       break;
 
     case kPracticeTeamNumber:
-      elevator_params->zeroing_constants.measured_absolute_position = 0.132216;
-      elevator->potentiometer_offset = -0.022320 + 0.020567 - 0.022355 - 0.006497 + 0.019690 + 0.009151;
+      elevator_params->zeroing_constants.measured_absolute_position = 0.141526;
+      elevator->potentiometer_offset = -0.022320 + 0.020567 - 0.022355 - 0.006497 + 0.019690 + 0.009151 - 0.007513;
 
       intake->zeroing_constants.measured_absolute_position = 1.756847;
 
