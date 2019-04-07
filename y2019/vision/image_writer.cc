670d5ac2673f92eba70de69746b43315bfe60da8
Alex Perry <alex.perry96@gmail.com>
2019-04-07 14:20:31 -0700

    switch image_writer to glog
    Change-Id: I286751f0f5090d2b7361eb833ea62f25b510d542

--- y2019/vision/image_writer.cc
+++ y2019/vision/image_writer.cc
@@ -1,19 +1,19 @@
 #include <fstream>
 #include <sys/stat.h>
 
-#include "aos/logging/logging.h"
+#include "glog/logging.h"
 #include "y2019/vision/image_writer.h"
 
 namespace y2019 {
 namespace vision {
 
 ImageWriter::ImageWriter() {
-  LOG(INFO, "Initializing image writer\n");
+  LOG(INFO) <<  "Initializing image writer";
   SetDirPath();
 }
 
 void ImageWriter::WriteImage(::aos::vision::DataRef data) {
-  LOG(INFO, "Writing image %d", image_count_);
+  LOG(INFO) << "Writing image " << image_count_;
   std::ofstream ofs(
       dir_path_ + file_prefix_ + std::to_string(image_count_) + ".yuyv",
       std::ofstream::out);
@@ -29,7 +29,7 @@ void ImageWriter::SetDirPath() {
     std::string option = base_path + std::to_string(i);
     if (stat(option.c_str(), &st) != 0) {
       file_prefix_ = option + "/";
-      LOG(INFO, "Writing to %s\n", file_prefix_.c_str());
+      LOG(INFO) << "Writing to " << file_prefix_.c_str();
       mkdir(file_prefix_.c_str(), 0777);
       break;
     }
