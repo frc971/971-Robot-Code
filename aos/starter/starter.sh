540089c70720edf44a7bfb347632872a49a49f5a
Austin Schuh <austin.linux@gmail.com>
2019-12-29 20:36:16 -0800

    Improve starter.sh script
    stderr seems to be getting lost.  Redirect it to stdout so it becomes
    visible.
    
    While we are here, don't set PATH every time...  Also, symlink the
    stderr log file into /tmp/ so it is easy to find.
    
    Change-Id: I2ea1fb6b77832b115e45dcf097cba2d969026f43

--- aos/starter/starter.sh
+++ aos/starter/starter.sh
@@ -3,9 +3,10 @@
 # NI already has a core pattern, so we probably shouldn't change it.
 #echo '/home/driver/tmp/robot_logs/%e-%s-%p-%t.coredump' > /proc/sys/kernel/core_pattern
 
-ROBOT_CODE=/home/admin/robot_code
-cd $ROBOT_CODE
+ln -s /var/local/natinst/log/FRC_UserProgram.log /tmp/FRC_UserProgram.log
+ROBOT_CODE="/home/admin/robot_code"
+cd "${ROBOT_CODE}"
+export PATH="${PATH}:${ROBOT_CODE}"
 while true; do
-	export PATH=$PATH:$ROBOT_CODE
-	starter_exe $ROBOT_CODE/start_list.txt
+	starter_exe $ROBOT_CODE/start_list.txt 2>&1
 done
