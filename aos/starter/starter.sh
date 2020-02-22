ab8c0c55c91c9ed0abb9c4e4f7aea721739bf9b1
Austin Schuh <austin.linux@gmail.com>
2020-02-22 13:42:37 -0800

    Add scripts to modify a root filesystem for the vision pi
    Change-Id: I6bbcafa92d8233914ce47c11f35d2a9dd24a1489

--- aos/starter/starter.sh
+++ aos/starter/starter.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# NI already has a core pattern, so we probably shouldn't change it.
-#echo '/home/driver/tmp/robot_logs/%e-%s-%p-%t.coredump' > /proc/sys/kernel/core_pattern
 
-ln -s /var/local/natinst/log/FRC_UserProgram.log /tmp/FRC_UserProgram.log
-ROBOT_CODE="/home/admin/robot_code"
+if [[ "$(hostname)" == "roboRIO"* ]]; then
+  ROBOT_CODE="/home/admin/robot_code"
+
+  ln -s /var/local/natinst/log/FRC_UserProgram.log /tmp/FRC_UserProgram.log
+  ln -s /var/local/natinst/log/FRC_UserProgram.log "${ROBOT_CODE}/FRC_UserProgram.log"
+else
+  ROBOT_CODE="${HOME}/robot_code"
+fi
+
 cd "${ROBOT_CODE}"
 export PATH="${PATH}:${ROBOT_CODE}"
 while true; do
---
+++ y2020/vision/rootfs/target_configure.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -ex
+
+mkdir -p /root/bin
+
+# Give it a static IP
+cp /tmp/dhcpcd.conf /etc/
+
+# And provide a script to change it.
+cp /tmp/change_hostname.sh /root/bin/
+chmod a+x /root/bin/change_hostname.sh
+
+chown -R pi.pi /home/pi/.ssh
+
+apt-get install -y vim-nox \
+  git \
+  cpufrequtils
+
+echo 'GOVERNOR="performance"' > /etc/default/cpufrequtils
+
+# Add a .bashrc and friends for root.
+if [[ ! -e "/root/.dotfiles" ]]; then
+  cd /root/
+  git clone --separate-git-dir=/root/.dotfiles https://github.com/AustinSchuh/.dotfiles.git tmpdotfiles
+  rsync --recursive --verbose --exclude '.git' tmpdotfiles/ /root/
+  rm -r tmpdotfiles
+  git --git-dir=/root/.dotfiles/ --work-tree=/root/ config --local status.showUntrackedFiles no
+  # Run the vundle installer which installs plugins on the first run of vim.
+  vim -c ":qa!"
+fi
+
+# Add a .bashrc and friends for pi.
+if [[ ! -e "/home/pi/.dotfiles" ]]; then
+  cd /home/pi/
+  su -c "git clone --separate-git-dir=/home/pi/.dotfiles https://github.com/AustinSchuh/.dotfiles.git tmpdotfiles" pi
+  su -c "rsync --recursive --verbose --exclude '.git' tmpdotfiles/ /home/pi/" pi
+  su -c "rm -r tmpdotfiles" pi
+  su -c "git --git-dir=/home/pi/.dotfiles/ --work-tree=/home/pi/ config --local status.showUntrackedFiles no" pi
+  # Run the vundle installer which installs plugins on the first run of vim.
+  su -c "vim -c ':qa!'" pi
+fi
+
+# Make SSH work and not complain about pi being the username and pw still.
+rm -f /etc/profile.d/sshpwd.sh
+rm -f /etc/profile.d/wifi-check.sh
+
+systemctl enable ssh.service
+
+# Default us to pi-8971-1
+/root/bin/change_hostname.sh pi-8971-1
