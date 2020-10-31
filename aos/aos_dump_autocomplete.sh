e6f5bef40348b3a68a6e730e076040f8ec751eb4
Tyler Chatow <tchatow@gmail.com>
2020-10-31 14:22:04 -0700

    Add aos_dump autocomplete script
    Change-Id: I8468af5774cbd928e423f857a1dd2d9dcf9bb94f

---
+++ aos/aos_dump_autocomplete.sh
@@ -0,0 +1,7 @@
+_aosdump_completions() {
+  eval "$($COMP_LINE --_bash_autocomplete --_bash_autocomplete_word=${COMP_WORDS[COMP_CWORD]})"
+  return 0;
+}
+
+complete -F _aosdump_completions -o default aos_dump
+complete -F _aosdump_completions -o default aos_dump.stripped
