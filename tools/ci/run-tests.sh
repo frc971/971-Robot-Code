4086152b5f7399349a91e93d9afdaa407daf3ef4
Austin Schuh <austin.linux@gmail.com>
2020-11-28 14:53:53 -0800

    Don't require bazel to be installed
    We've got tools/bazel.  The indirection doesn't help and requires the
    server to have more things setup correctly.
    
    Change-Id: I36d0d7ce1cd181021b83a9c0826c3052c0794f1a

--- tools/ci/run-tests.sh
+++ tools/ci/run-tests.sh
@@ -14,23 +14,23 @@ readonly COMMON='-c opt --stamp=no --curses=no --color=no --symlink_prefix=/'
 
 # Include --config=eigen to enable Eigen assertions so that we catch potential
 # bugs with Eigen.
-bazel --output_base=../k8_output_base test \
+tools/bazel --output_base=../k8_output_base test \
     ${COMMON} \
     --cpu=k8 \
     --config=eigen \
     ${TARGETS}
 
-bazel --output_base=../roborio_output_base build \
+tools/bazel --output_base=../roborio_output_base build \
     ${COMMON} \
     --cpu=roborio \
     ${TARGETS}
 
-bazel --output_base=../armhf-debian_output_base build \
+tools/bazel --output_base=../armhf-debian_output_base build \
     ${COMMON} \
     --cpu=armhf-debian \
     ${TARGETS}
 
-bazel --output_base=../cortex-m4f_output_base build \
+tools/bazel --output_base=../cortex-m4f_output_base build \
     ${COMMON} \
     --cpu=cortex-m4f \
     ${M4F_TARGETS}
