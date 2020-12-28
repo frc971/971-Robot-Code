512b98f16a820628f988987494e61aacede6e6c8
Austin Schuh <austin.linux@gmail.com>
2020-12-28 13:12:16 -0800

    Clean the disk cache after every build
    This will prevent disk usage explosion
    
    Change-Id: I1d55ee17898e56afd22a52f3efb1aa29f3a5c4be

--- tools/ci/clean-disk.sh
+++ tools/ci/clean-disk.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Retrieve Jenkins node name or set a test value
-NODE_NAME=${NODE_NAME:-"TEST971"}
-#Set default for disk utilisation
-DMAX=${1:-80%}
-#Retrieve disk usages in percentage
-DSIZE=$(df -hlP /home/jenkins | sed 1d | awk '{print $5}')
+# Set default for disk utilisation
+readonly DMAX="${1:-80%}"
+# Cache folder
+readonly CACHE_FOLDER="${HOME}/.cache/bazel/disk_cache/"
+# Retrieve disk usages in percentage
+readonly DSIZE="$(df -hlP "${CACHE_FOLDER}" | sed 1d | awk '{print $5}')"
 
-if [[ $DSIZE>$DMAX ]]; then
-		echo $NODE_NAME": Disk over "$DMAX" Clean up needed on node."
-		rm -rf ~jenkins/.cache/bazel/disk_cache;
+if [[ ${DSIZE} > ${DMAX} ]]; then
+  echo "$(hostname): Disk over ${DMAX} Clean up needed on node."
+  rm -rf "${CACHE_FOLDER}"
 else
-    echo $NODE_NAME": No clean up needed. Disk usage is at: "$DSIZE
+  echo "$(hostname): No clean up needed. Disk usage is at: ${DSIZE}"
 fi
