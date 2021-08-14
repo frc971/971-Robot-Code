c59171bf127909a6f7d111f2ecd9a447031212b0
Sabina Leaver <100027607@mvla.net>
2021-08-14 14:22:18 -0700

    Implementing the 3rd robot superstructure
    Signed-off-by: Sabina Leaver <100027607@mvla.net>
    Change-Id: Ifae4ab862d15e52d6010de7da5a87d2a0c94e48d
    Signed-off-by: Sabina Leaver <100027607@mvla.net>

--- y2021_bot3/control_loops/superstructure/superstructure.cc
+++ y2021_bot3/control_loops/superstructure/superstructure.cc
@@ -16,7 +16,7 @@ Superstructure::Superstructure(::aos::EventLoop *event_loop,
   event_loop->SetRuntimeRealtimePriority(30);
 }
 
-void Superstructure::RunIteration(const Goal * /*unsafe_goal*/,
+void Superstructure::RunIteration(const Goal *unsafe_goal,
                                   const Position * /*position*/,
                                   aos::Sender<Output>::Builder *output,
                                   aos::Sender<Status>::Builder *status) {
@@ -24,8 +24,13 @@ void Superstructure::RunIteration(const Goal * /*unsafe_goal*/,
     AOS_LOG(ERROR, "WPILib reset, restarting\n");
   }
 
-  if (output != nullptr) {
+  if (output != nullptr && unsafe_goal != nullptr) {
     OutputT output_struct;
+
+    output_struct.intake_volts =
+        std::clamp(unsafe_goal->intake_speed(), -12.0, 12.0);
+    output_struct.outtake_volts =
+        std::clamp(unsafe_goal->outtake_speed(), -12.0, 12.0);
     output->Send(Output::Pack(*output->fbb(), &output_struct));
   }
 
@@ -33,6 +38,11 @@ void Superstructure::RunIteration(const Goal * /*unsafe_goal*/,
 
   status_builder.add_zeroed(true);
   status_builder.add_estopped(false);
+
+  if (unsafe_goal != nullptr) {
+    status_builder.add_intake_speed(unsafe_goal->intake_speed());
+    status_builder.add_outtake_speed(unsafe_goal->outtake_speed());
+  }
 
   status->Send(status_builder.Finish());
 }
