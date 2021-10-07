58f04b719b09697b8c93688c41e4a1e36357e66c
Sabina Leaver <100027607@mvla.net>
2021-10-06 20:52:09 -0700

    Adding a plot for the superstructure
    Signed-off-by: Sabina Leaver <100027607@mvla.net>
    Change-Id: Ib94a948443015525428145741f9062272ed7ca18
    Signed-off-by: Sabina Leaver <100027607@mvla.net>

--- y2021_bot3/control_loops/superstructure/superstructure_lib_test.cc
+++ y2021_bot3/control_loops/superstructure/superstructure_lib_test.cc
@@ -1,13 +1,16 @@
 #include <chrono>
 #include <memory>
 
-#include "aos/events/logging/log_writer.h"
 #include "frc971/control_loops/capped_test_plant.h"
 #include "frc971/control_loops/control_loop_test.h"
 #include "frc971/control_loops/position_sensor_sim.h"
 #include "frc971/control_loops/team_number_test_environment.h"
 #include "gtest/gtest.h"
 #include "y2021_bot3/control_loops/superstructure/superstructure.h"
+#include "aos/events/logging/log_writer.h"
+
+DEFINE_string(output_folder, "",
+              "If set, logs all channels to the provided logfile.");
 
 namespace y2021_bot3 {
 namespace control_loops {
@@ -40,6 +43,13 @@ class SuperstructureTest : public ::frc971::testing::ControlLoopTest {
 
     phased_loop_handle_ = test_event_loop_->AddPhasedLoop(
         [this](int) { SendPositionMessage(); }, dt());
+
+    if (!FLAGS_output_folder.empty()) {
+      unlink(FLAGS_output_folder.c_str());
+      logger_event_loop_ = MakeEventLoop("logger", roborio_);
+      logger_ = std::make_unique<aos::logger::Logger>(logger_event_loop_.get());
+      logger_->StartLoggingLocalNamerOnRun(FLAGS_output_folder);
+    }
   }
 
   void VerifyResults(double intake_voltage, double outtake_voltage,
@@ -65,6 +75,9 @@ class SuperstructureTest : public ::frc971::testing::ControlLoopTest {
     builder.Send(position_builder.Finish());
   }
 
+  // Because the third robot is single node, the roborio node is nullptr
+  const aos::Node *const roborio_ = nullptr;
+
   ::std::unique_ptr<::aos::EventLoop> superstructure_event_loop;
   ::y2021_bot3::control_loops::superstructure::Superstructure superstructure_;
   ::std::unique_ptr<::aos::EventLoop> test_event_loop_;
@@ -75,6 +88,8 @@ class SuperstructureTest : public ::frc971::testing::ControlLoopTest {
   ::aos::Fetcher<Output> superstructure_output_fetcher_;
   ::aos::Fetcher<Position> superstructure_position_fetcher_;
   ::aos::Sender<Position> superstructure_position_sender_;
+  std::unique_ptr<aos::EventLoop> logger_event_loop_;
+  std::unique_ptr<aos::logger::Logger> logger_;
 };
 
 // Tests running the intake and outtake separately
@@ -165,6 +180,25 @@ TEST_F(SuperstructureTest, Disabled) {
   SetEnabled(false);
   RunFor(dt() * 2);
   VerifyResults(0.0, 0.0, 0.0, 6.0, 5.0, 4.0);
+}
+
+TEST_F(SuperstructureTest, PlotterTest) {
+  double speed = 10.0;
+  test_event_loop_->AddPhasedLoop(
+    [&](int) {
+      auto builder = superstructure_goal_sender_.MakeBuilder();
+      Goal::Builder goal_builder = builder.MakeBuilder<Goal>();
+      goal_builder.add_intake_speed(speed);
+      goal_builder.add_outtake_speed(speed);
+      goal_builder.add_climber_speed(speed);
+      ASSERT_TRUE(builder.Send(goal_builder.Finish()));
+      speed += .001;
+      if (speed >= 12) {
+        speed = -12;
+      }
+    },
+    frc971::controls::kLoopFrequency);
+  RunFor(std::chrono::seconds(10));
 }
 
 }  // namespace testing
