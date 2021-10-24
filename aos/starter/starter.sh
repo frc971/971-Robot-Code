66e00056b9f23bd7e644f5a1cf95d2c933f06ea1
Austin Schuh <austin.linux@gmail.com>
2021-10-23 22:00:52 -0700

    Make starter restart correctly on pi's
    This configures systemd much better so it automatically restarts and
    gets the kill signals right.
    
    Change-Id: I43576758f1b137ca24ad1ee960dd8715143aa7e6
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- aos/starter/starter.sh
+++ aos/starter/starter.sh
@@ -8,8 +8,9 @@ if [[ "$(hostname)" == "roboRIO"* ]]; then
   ln -s /var/local/natinst/log/FRC_UserProgram.log /tmp/FRC_UserProgram.log
   ln -s /var/local/natinst/log/FRC_UserProgram.log "${ROBOT_CODE}/FRC_UserProgram.log"
 elif [[ "$(hostname)" == "pi-"* ]]; then
-  ROBOT_CODE="/home/pi/robot_code"
-
+  # We have systemd configured to handle restarting, so just exec.
+  export PATH="${PATH}:/home/pi/robot_code"
+  exec starterd
 else
   ROBOT_CODE="${HOME}/robot_code"
 fi
