Re-fixing the pitch calculation for extrinsics
I fixed the pitch calculation for the camera, but forgot to update
the sign of the pitch values (should be negative)

Change-Id: I24cd4523cfdb2be6611f227a7ff05f759f21d299
Signed-off-by: Jim Ostrowski <yimmy13@gmail.com>

y2020/vision/tools/python_code/camera_definition.py
@@ -74,7 +74,7 @@ def compute_extrinsic(camera_pitch, camera_yaw, T_camera, is_turret):
 
 def compute_extrinsic_by_pi(pi_number):
     # Defaults for non-turret camera
-    camera_pitch = 20.0 * np.pi / 180.0
+    camera_pitch = -20.0 * np.pi / 180.0
     camera_yaw = 0.0
     is_turret = False
     # Default camera location to robot origin
@@ -82,7 +82,7 @@ def compute_extrinsic_by_pi(pi_number):
 
     if pi_number == "pi1":
         # This is the turret camera
-        camera_pitch = 10.0 * np.pi / 180.0
+        camera_pitch = -10.0 * np.pi / 180.0
         is_turret = True
         T = np.array([7.5 * 0.0254, -5.5 * 0.0254, 41.0 * 0.0254])
     elif pi_number == "pi2":