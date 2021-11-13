b24e90502a4fd275b6a4403cf7438f3bcd56a8a0
Austin Schuh <austin.linux@gmail.com>
2021-11-12 19:53:33 -0800

    Extend the table a bit further
    Vision sometimes reports us being a bit far.  Ignore the error and use
    the same far shot.
    
    Change-Id: Ie1d5f2a63f946afec7fc491e3ac42a3c4c14f311
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- y2020/constants.h
+++ y2020/constants.h
@@ -69,9 +69,9 @@ struct Values {
   static constexpr ::frc971::constants::Range kHoodRange() {
     return ::frc971::constants::Range{
         -0.01,  // Back Hard
-        0.675,   // Front Hard
+        0.685,   // Front Hard
         0.00,   // Back Soft
-        0.67    // Front Soft
+        0.68    // Front Soft
     };
   }
 
