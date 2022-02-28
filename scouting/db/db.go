1e6c0a9ea90796a14c678e71ebb153980cc4e8df
Philipp Schrader <philipp.schrader@gmail.com>
2022-02-27 23:30:57 -0800

    Export the public fields in scouting/db/db.go
    When I tried making use of the database datain `requests.go`, the
    compiler was getting really unhappy:
    
        scouting/webserver/requests/requests.go:127:22: match.matchNumber undefined (cannot refer to unexported field or method matchNumber)
        scouting/webserver/requests/requests.go:128:22: match.round undefined (cannot refer to unexported field or method round)
        scouting/webserver/requests/requests.go:129:22: match.compLevel undefined (cannot refer to unexported field or method compLevel)
        scouting/webserver/requests/requests.go:130:22: match.r1 undefined (cannot refer to unexported field or method r1)
        scouting/webserver/requests/requests.go:131:22: match.r2 undefined (cannot refer to unexported field or method r2)
        scouting/webserver/requests/requests.go:132:22: match.r3 undefined (cannot refer to unexported field or method r3)
        scouting/webserver/requests/requests.go:133:22: match.b1 undefined (cannot refer to unexported field or method b1)
        scouting/webserver/requests/requests.go:134:22: match.b2 undefined (cannot refer to unexported field or method b2)
    
    The answer here is to export the interesting fields. The ID fields can
    stay private. We have no need for them on the webserver side.
    
    A future patch will export the interesting fields in the `Stats`
    struct.
    
    Signed-off-by: Philipp Schrader <philipp.schrader@gmail.com>
    Change-Id: I9ffcbbf206899af652605f6e28bc60dbfb9ad728

--- scouting/db/db.go
+++ scouting/db/db.go
@@ -12,15 +12,15 @@ type Database struct {
 }
 
 type Match struct {
-	matchNumber, round     int
-	compLevel              string
-	r1, r2, r3, b1, b2, b3 int
+	MatchNumber, Round     int
+	CompLevel              string
+	R1, R2, R3, B1, B2, B3 int
 	// Each of these variables holds the matchID of the corresponding Stats row
 	r1ID, r2ID, r3ID, b1ID, b2ID, b3ID int
 }
 
 type Stats struct {
-	teamNumber, matchNumber                                      int
+	teamNumber, MatchNumber                                      int
 	shotsMissed, upperGoalShots, lowerGoalShots                  int
 	shotsMissedAuto, upperGoalAuto, lowerGoalAuto, playedDefense int
 	climbing                                                     int
@@ -32,14 +32,14 @@ func NewDatabase(path string) (*Database, error) {
 	database := new(Database)
 	database.DB, _ = sql.Open("sqlite3", path)
 	statement, error_ := database.Prepare("CREATE TABLE IF NOT EXISTS matches " +
-		"(id INTEGER PRIMARY KEY, matchNumber INTEGER, round INTEGER, compLevel INTEGER, r1 INTEGER, r2 INTEGER, r3 INTEGER, b1 INTEGER, b2 INTEGER, b3 INTEGER, r1ID INTEGER, r2ID INTEGER, r3ID INTEGER, b1ID INTEGER, b2ID INTEGER, b3ID INTEGER)")
+		"(id INTEGER PRIMARY KEY, MatchNumber INTEGER, Round INTEGER, CompLevel INTEGER, R1 INTEGER, R2 INTEGER, R3 INTEGER, B1 INTEGER, B2 INTEGER, B3 INTEGER, r1ID INTEGER, r2ID INTEGER, r3ID INTEGER, b1ID INTEGER, b2ID INTEGER, b3ID INTEGER)")
 	defer statement.Close()
 	if error_ != nil {
 		fmt.Println(error_)
 		return nil, error_
 	}
 	_, error_ = statement.Exec()
-	statement, error_ = database.Prepare("CREATE TABLE IF NOT EXISTS team_match_stats (id INTEGER PRIMARY KEY, teamNumber INTEGER, matchNumber DOUBLE, shotsMissed INTEGER, upperGoalShots INTEGER, lowerGoalShots INTEGER, shotsMissedAuto INTEGER, upperGoalAuto INTEGER, lowerGoalAuto INTEGER, playedDefense INTEGER, climbing INTEGER)")
+	statement, error_ = database.Prepare("CREATE TABLE IF NOT EXISTS team_match_stats (id INTEGER PRIMARY KEY, teamNumber INTEGER, MatchNumber DOUBLE, shotsMissed INTEGER, upperGoalShots INTEGER, lowerGoalShots INTEGER, shotsMissedAuto INTEGER, upperGoalAuto INTEGER, lowerGoalAuto INTEGER, playedDefense INTEGER, climbing INTEGER)")
 	defer statement.Close()
 	if error_ != nil {
 		fmt.Println(error_)
@@ -67,28 +67,28 @@ func (database *Database) Delete() error {
 
 // This function will also populate the Stats table with six empty rows every time a match is added
 func (database *Database) AddToMatch(m Match) error {
-	statement, error_ := database.Prepare("INSERT INTO team_match_stats(teamNumber, matchNumber, shotsMissed, upperGoalShots, lowerGoalShots, shotsMissedAuto, upperGoalAuto, lowerGoalAuto, playedDefense, climbing) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	statement, error_ := database.Prepare("INSERT INTO team_match_stats(teamNumber, MatchNumber, shotsMissed, upperGoalShots, lowerGoalShots, shotsMissedAuto, upperGoalAuto, lowerGoalAuto, playedDefense, climbing) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	defer statement.Close()
 	if error_ != nil {
 		fmt.Println("failed to prepare stats database:", error_)
 		return (error_)
 	}
 	var rowIds [6]int64
-	for i, teamNumber := range []int{m.r1, m.r2, m.r3, m.b1, m.b2, m.b3} {
-		result, error_ := statement.Exec(teamNumber, m.matchNumber, 0, 0, 0, 0, 0, 0, 0, 0)
+	for i, teamNumber := range []int{m.R1, m.R2, m.R3, m.B1, m.B2, m.B3} {
+		result, error_ := statement.Exec(teamNumber, m.MatchNumber, 0, 0, 0, 0, 0, 0, 0, 0)
 		if error_ != nil {
 			fmt.Println("failed to execute statement 2:", error_)
 			return (error_)
 		}
 		rowIds[i], error_ = result.LastInsertId()
 	}
-	statement, error_ = database.Prepare("INSERT INTO matches(matchNumber, round, compLevel, r1, r2, r3, b1, b2, b3, r1ID, r2ID, r3ID, b1ID, b2ID, b3ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+	statement, error_ = database.Prepare("INSERT INTO matches(MatchNumber, Round, CompLevel, R1, R2, R3, B1, B2, B3, r1ID, r2ID, r3ID, b1ID, b2ID, b3ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
 	defer statement.Close()
 	if error_ != nil {
 		fmt.Println("failed to prepare match database:", error_)
 		return (error_)
 	}
-	_, error_ = statement.Exec(m.matchNumber, m.round, m.compLevel, m.r1, m.r2, m.r3, m.b1, m.b2, m.b3, rowIds[0], rowIds[1], rowIds[2], rowIds[3], rowIds[4], rowIds[5])
+	_, error_ = statement.Exec(m.MatchNumber, m.Round, m.CompLevel, m.R1, m.R2, m.R3, m.B1, m.B2, m.B3, rowIds[0], rowIds[1], rowIds[2], rowIds[3], rowIds[4], rowIds[5])
 	if error_ != nil {
 		fmt.Println(error_)
 		return (error_)
@@ -97,12 +97,12 @@ func (database *Database) AddToMatch(m Match) error {
 }
 
 func (database *Database) AddToStats(s Stats) error {
-	statement, error_ := database.Prepare("UPDATE team_match_stats SET teamNumber = ?, matchNumber = ?, shotsMissed = ?, upperGoalShots = ?, lowerGoalShots = ?, shotsMissedAuto = ?, upperGoalAuto = ?, lowerGoalAuto = ?, playedDefense = ?, climbing = ? WHERE matchNumber = ? AND teamNumber = ?")
+	statement, error_ := database.Prepare("UPDATE team_match_stats SET teamNumber = ?, MatchNumber = ?, shotsMissed = ?, upperGoalShots = ?, lowerGoalShots = ?, shotsMissedAuto = ?, upperGoalAuto = ?, lowerGoalAuto = ?, playedDefense = ?, climbing = ? WHERE MatchNumber = ? AND teamNumber = ?")
 	if error_ != nil {
 		fmt.Println(error_)
 		return (error_)
 	}
-	_, error_ = statement.Exec(s.teamNumber, s.matchNumber, s.shotsMissed, s.upperGoalShots, s.lowerGoalShots, s.shotsMissedAuto, s.upperGoalAuto, s.lowerGoalAuto, s.playedDefense, s.climbing, s.matchNumber, s.teamNumber)
+	_, error_ = statement.Exec(s.teamNumber, s.MatchNumber, s.shotsMissed, s.upperGoalShots, s.lowerGoalShots, s.shotsMissedAuto, s.upperGoalAuto, s.lowerGoalAuto, s.playedDefense, s.climbing, s.MatchNumber, s.teamNumber)
 	if error_ != nil {
 		fmt.Println(error_)
 		return (error_)
@@ -117,7 +117,7 @@ func (database *Database) ReturnMatches() ([]Match, error) {
 	for rows.Next() {
 		var match Match
 		var id int
-		error_ := rows.Scan(&id, &match.matchNumber, &match.round, &match.compLevel, &match.r1, &match.r2, &match.r3, &match.b1, &match.b2, &match.b3, &match.r1ID, &match.r2ID, &match.r3ID, &match.b1ID, &match.b2ID, &match.b3ID)
+		error_ := rows.Scan(&id, &match.MatchNumber, &match.Round, &match.CompLevel, &match.R1, &match.R2, &match.R3, &match.B1, &match.B2, &match.B3, &match.r1ID, &match.r2ID, &match.r3ID, &match.b1ID, &match.b2ID, &match.b3ID)
 		if error_ != nil {
 			fmt.Println(nil, error_)
 			return nil, error_
@@ -134,7 +134,7 @@ func (database *Database) ReturnStats() ([]Stats, error) {
 	var id int
 	for rows.Next() {
 		var team Stats
-		error_ := rows.Scan(&id, &team.teamNumber, &team.matchNumber, &team.shotsMissed, &team.upperGoalShots, &team.lowerGoalShots, &team.shotsMissedAuto, &team.upperGoalAuto, &team.lowerGoalAuto, &team.playedDefense, &team.climbing)
+		error_ := rows.Scan(&id, &team.teamNumber, &team.MatchNumber, &team.shotsMissed, &team.upperGoalShots, &team.lowerGoalShots, &team.shotsMissedAuto, &team.upperGoalAuto, &team.lowerGoalAuto, &team.playedDefense, &team.climbing)
 		if error_ != nil {
 			fmt.Println(error_)
 			return nil, error_
@@ -145,7 +145,7 @@ func (database *Database) ReturnStats() ([]Stats, error) {
 }
 
 func (database *Database) QueryMatches(teamNumber_ int) ([]Match, error) {
-	rows, error_ := database.Query("SELECT * FROM matches WHERE r1 = ? OR r2 = ? OR r3 = ? OR b1 = ? OR b2 = ? OR b3 = ?", teamNumber_, teamNumber_, teamNumber_, teamNumber_, teamNumber_, teamNumber_)
+	rows, error_ := database.Query("SELECT * FROM matches WHERE R1 = ? OR R2 = ? OR R3 = ? OR B1 = ? OR B2 = ? OR B3 = ?", teamNumber_, teamNumber_, teamNumber_, teamNumber_, teamNumber_, teamNumber_)
 	if error_ != nil {
 		fmt.Println("failed to execute statement 1:", error_)
 		return nil, error_
@@ -155,7 +155,7 @@ func (database *Database) QueryMatches(teamNumber_ int) ([]Match, error) {
 	var id int
 	for rows.Next() {
 		var match Match
-		rows.Scan(&id, &match.matchNumber, &match.round, &match.compLevel, &match.r1, &match.r2, &match.r3, &match.b1, &match.b2, &match.b3, &match.r1ID, &match.r2ID, &match.r3ID, &match.b1ID, &match.b2ID, &match.b3ID)
+		rows.Scan(&id, &match.MatchNumber, &match.Round, &match.CompLevel, &match.R1, &match.R2, &match.R3, &match.B1, &match.B2, &match.B3, &match.r1ID, &match.r2ID, &match.r3ID, &match.b1ID, &match.b2ID, &match.b3ID)
 		matches = append(matches, match)
 	}
 	return matches, nil
@@ -172,7 +172,7 @@ func (database *Database) QueryStats(teamNumber_ int) ([]Stats, error) {
 	for rows.Next() {
 		var team Stats
 		var id int
-		error_ = rows.Scan(&id, &team.teamNumber, &team.matchNumber, &team.shotsMissed,
+		error_ = rows.Scan(&id, &team.teamNumber, &team.MatchNumber, &team.shotsMissed,
 			&team.upperGoalShots, &team.lowerGoalShots, &team.shotsMissedAuto, &team.upperGoalAuto,
 			&team.lowerGoalAuto, &team.playedDefense, &team.climbing)
 		teams = append(teams, team)
