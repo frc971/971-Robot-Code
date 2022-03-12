81e5063d4ecfd5e5b4bff5995f598c80d289afa6
Ravago Jones <ravagojones@gmail.com>
2022-03-11 16:23:51 -0800

    Updated 2022 Autonomous
    Change-Id: I68dcef53387c909a6014b6ee7d46c2b934695beb
    Signed-off-by: Vinay Siva <100024232@mvla.net>
    Signed-off-by: Griffin Bui <griffinbui+gerrit@gmail.com>
    Signed-off-by: Ravago Jones <ravagojones@gmail.com>
    Signed-off-by: milind-u <milind.upadhyay@gmail.com>

--- y2022/actors/auto_splines.cc
+++ y2022/actors/auto_splines.cc
@@ -19,84 +19,35 @@ void MaybeFlipSpline(
   }
 }
 
-flatbuffers::Offset<frc971::MultiSpline> AutonomousSplines::BasicSSpline(
-    aos::Sender<frc971::control_loops::drivetrain::Goal>::Builder *builder) {
-  flatbuffers::Offset<frc971::Constraint> longitudinal_constraint_offset;
-  flatbuffers::Offset<frc971::Constraint> lateral_constraint_offset;
-  flatbuffers::Offset<frc971::Constraint> voltage_constraint_offset;
+flatbuffers::Offset<frc971::MultiSpline> FixSpline(
+    aos::Sender<frc971::control_loops::drivetrain::SplineGoal>::Builder
+        *builder,
+    flatbuffers::Offset<frc971::MultiSpline> spline_offset,
+    aos::Alliance alliance) {
+  frc971::MultiSpline *spline =
+      GetMutableTemporaryPointer(*builder->fbb(), spline_offset);
+  flatbuffers::Vector<float> *spline_x = spline->mutable_spline_x();
+  flatbuffers::Vector<float> *spline_y = spline->mutable_spline_y();
 
-  {
-    frc971::Constraint::Builder longitudinal_constraint_builder =
-        builder->MakeBuilder<frc971::Constraint>();
-    longitudinal_constraint_builder.add_constraint_type(
-        frc971::ConstraintType::LONGITUDINAL_ACCELERATION);
-    longitudinal_constraint_builder.add_value(1.0);
-    longitudinal_constraint_offset = longitudinal_constraint_builder.Finish();
+  if (alliance == aos::Alliance::kBlue) {
+    for (size_t ii = 0; ii < spline_x->size(); ++ii) {
+      spline_x->Mutate(ii, -spline_x->Get(ii));
+    }
+    for (size_t ii = 0; ii < spline_y->size(); ++ii) {
+      spline_y->Mutate(ii, -spline_y->Get(ii));
+    }
   }
-
-  {
-    frc971::Constraint::Builder lateral_constraint_builder =
-        builder->MakeBuilder<frc971::Constraint>();
-    lateral_constraint_builder.add_constraint_type(
-        frc971::ConstraintType::LATERAL_ACCELERATION);
-    lateral_constraint_builder.add_value(1.0);
-    lateral_constraint_offset = lateral_constraint_builder.Finish();
-  }
-
-  {
-    frc971::Constraint::Builder voltage_constraint_builder =
-        builder->MakeBuilder<frc971::Constraint>();
-    voltage_constraint_builder.add_constraint_type(
-        frc971::ConstraintType::VOLTAGE);
-    voltage_constraint_builder.add_value(6.0);
-    voltage_constraint_offset = voltage_constraint_builder.Finish();
-  }
-
-  flatbuffers::Offset<
-      flatbuffers::Vector<flatbuffers::Offset<frc971::Constraint>>>
-      constraints_offset =
-          builder->fbb()->CreateVector<flatbuffers::Offset<frc971::Constraint>>(
-              {longitudinal_constraint_offset, lateral_constraint_offset,
-               voltage_constraint_offset});
-
-  const float startx = 0.4;
-  const float starty = 3.4;
-  flatbuffers::Offset<flatbuffers::Vector<float>> spline_x_offset =
-      builder->fbb()->CreateVector<float>({0.0f + startx, 0.6f + startx,
-                                           0.6f + startx, 0.4f + startx,
-                                           0.4f + startx, 1.0f + startx});
-  flatbuffers::Offset<flatbuffers::Vector<float>> spline_y_offset =
-      builder->fbb()->CreateVector<float>({starty - 0.0f, starty - 0.0f,
-                                           starty - 0.3f, starty - 0.7f,
-                                           starty - 1.0f, starty - 1.0f});
-
-  frc971::MultiSpline::Builder multispline_builder =
-      builder->MakeBuilder<frc971::MultiSpline>();
-
-  multispline_builder.add_spline_count(1);
-  multispline_builder.add_constraints(constraints_offset);
-  multispline_builder.add_spline_x(spline_x_offset);
-  multispline_builder.add_spline_y(spline_y_offset);
-
-  return multispline_builder.Finish();
+  return spline_offset;
 }
 
-flatbuffers::Offset<frc971::MultiSpline> AutonomousSplines::StraightLine(
-    aos::Sender<frc971::control_loops::drivetrain::Goal>::Builder *builder) {
-  flatbuffers::Offset<flatbuffers::Vector<float>> spline_x_offset =
-      builder->fbb()->CreateVector<float>(
-          {-12.3, -11.9, -11.5, -11.1, -10.6, -10.0});
-  flatbuffers::Offset<flatbuffers::Vector<float>> spline_y_offset =
-      builder->fbb()->CreateVector<float>({1.25, 1.25, 1.25, 1.25, 1.25, 1.25});
-
-  frc971::MultiSpline::Builder multispline_builder =
-      builder->MakeBuilder<frc971::MultiSpline>();
-
-  multispline_builder.add_spline_count(1);
-  multispline_builder.add_spline_x(spline_x_offset);
-  multispline_builder.add_spline_y(spline_y_offset);
-
-  return multispline_builder.Finish();
+flatbuffers::Offset<frc971::MultiSpline> AutonomousSplines::TestSpline(
+    aos::Sender<frc971::control_loops::drivetrain::SplineGoal>::Builder
+        *builder,
+    aos::Alliance alliance) {
+  return FixSpline(
+      builder,
+      aos::CopyFlatBuffer<frc971::MultiSpline>(test_spline_, builder->fbb()),
+      alliance);
 }
 
 }  // namespace actors
