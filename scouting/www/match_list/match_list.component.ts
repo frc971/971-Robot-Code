89cd72196d55c8ceaf3eb53a26f19c53558edf73
Alex Perry <alex.perry96@gmail.com>
2022-04-06 20:55:37 -0700

    Create a service to fetch the match list.
    I plan to reuse the functionality for creating a notes scouting
    schedule.
    
    Change-Id: Ia449a6567ec63f5faca1af6e4a154cb77c359c88
    Signed-off-by: Alex Perry <alex.perry96@gmail.com>

--- scouting/www/match_list/match_list.component.ts
+++ scouting/www/match_list/match_list.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {ByteBuffer, Builder} from 'flatbuffers';
+import {Builder, ByteBuffer} from 'flatbuffers';
 import {ErrorResponse} from 'org_frc971/scouting/webserver/requests/messages/error_response_generated';
 import {RequestAllMatches} from 'org_frc971/scouting/webserver/requests/messages/request_all_matches_generated';
 import {
@@ -7,13 +7,13 @@ import {
   RequestAllMatchesResponse,
 } from 'org_frc971/scouting/webserver/requests/messages/request_all_matches_response_generated';
 
+import {MatchListRequestor} from '../rpc/match_list_requestor';
+
 type TeamInMatch = {
   teamNumber: number;
   matchNumber: number;
   compLevel: string;
 };
-
-const MATCH_TYPE_ORDERING = ['qm', 'ef', 'qf', 'sf', 'f'];
 
 @Component({
   selector: 'app-match-list',
@@ -25,6 +25,8 @@ export class MatchListComponent implements OnInit {
   progressMessage: string = '';
   errorMessage: string = '';
   matchList: Match[] = [];
+
+  constructor(private readonly matchListRequestor: MatchListRequestor) {}
 
   setTeamInMatch(teamInMatch: TeamInMatch) {
     this.selectedTeamEvent.emit(teamInMatch);
@@ -67,62 +69,15 @@ export class MatchListComponent implements OnInit {
   }
 
   async fetchMatchList() {
+    this.progressMessage = 'Fetching match list. Please be patient.';
     this.errorMessage = '';
 
-    const builder = new Builder();
-    RequestAllMatches.startRequestAllMatches(builder);
-    builder.finish(RequestAllMatches.endRequestAllMatches(builder));
-
-    this.progressMessage = 'Fetching match list. Please be patient.';
-
-    const buffer = builder.asUint8Array();
-    const res = await fetch('/requests/request/all_matches', {
-      method: 'POST',
-      body: buffer,
-    });
-
-    if (res.ok) {
-      const resBuffer = await res.arrayBuffer();
-      const fbBuffer = new ByteBuffer(new Uint8Array(resBuffer));
-      const parsedResponse =
-        RequestAllMatchesResponse.getRootAsRequestAllMatchesResponse(fbBuffer);
-
-      // Convert the flatbuffer list into an array. That's more useful.
-      this.matchList = [];
-      for (let i = 0; i < parsedResponse.matchListLength(); i++) {
-        this.matchList.push(parsedResponse.matchList(i));
-      }
-
-      // Sort the list so it is in chronological order.
-      this.matchList.sort((a, b) => {
-        const aMatchTypeIndex = MATCH_TYPE_ORDERING.indexOf(a.compLevel());
-        const bMatchTypeIndex = MATCH_TYPE_ORDERING.indexOf(b.compLevel());
-        if (aMatchTypeIndex < bMatchTypeIndex) {
-          return -1;
-        }
-        if (aMatchTypeIndex > bMatchTypeIndex) {
-          return 1;
-        }
-        const aMatchNumber = a.matchNumber();
-        const bMatchNumber = b.matchNumber();
-        if (aMatchNumber < bMatchNumber) {
-          return -1;
-        }
-        if (aMatchNumber > bMatchNumber) {
-          return 1;
-        }
-        return 0;
-      });
-
+    try {
+      this.matchList = await this.matchListRequestor.fetchMatchList();
       this.progressMessage = 'Successfully fetched match list.';
-    } else {
+    } catch (e) {
+      this.errorMessage = e;
       this.progressMessage = '';
-      const resBuffer = await res.arrayBuffer();
-      const fbBuffer = new ByteBuffer(new Uint8Array(resBuffer));
-      const parsedResponse = ErrorResponse.getRootAsErrorResponse(fbBuffer);
-
-      const errorMessage = parsedResponse.errorMessage();
-      this.errorMessage = `Received ${res.status} ${res.statusText}: "${errorMessage}"`;
     }
   }
 }
