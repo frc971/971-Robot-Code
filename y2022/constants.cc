4afefdb6d5038cc44d4c39b1dc50ffaae31f0467
Nathan Leong <100028864@mvla.net>
2022-12-21 21:17:19 -0800

    Fix potentiometer offset constants
    Fixed the potentiometer offset constants to work with
    superstructure_lib_test.
    
    Signed-off-by: Nathan Leong <100028864@mvla.net>
    Change-Id: I96ebc9cb6a0cbdd7129da4414fdc4e3af22042dc

--- y2022/constants.cc
+++ y2022/constants.cc
@@ -141,7 +141,7 @@ Values MakeValues(uint16_t team) {
 
       {3.60, {0.39, 20.65}},
       {4.50, {0.44, 22.3}},
-      {4.9, {0.43, 22.75}}, // up to here
+      {4.9, {0.43, 22.75}},  // up to here
       {5.4, {0.43, 23.85}},
 
       {6.0, {0.42, 25.3}},
@@ -190,21 +190,21 @@ Values MakeValues(uint16_t team) {
               {5, {4.0}},
           });
 
-      climber->potentiometer_offset = 0.0;
-      intake_front->potentiometer_offset = 0.0;
+      climber->potentiometer_offset = -0.035;
+      intake_front->potentiometer_offset = 3.122;
       intake_front->subsystem_params.zeroing_constants
-          .measured_absolute_position = 0.0;
-      intake_back->potentiometer_offset = 0.0;
+          .measured_absolute_position = 0.175;
+      intake_back->potentiometer_offset = 3.365;
       intake_back->subsystem_params.zeroing_constants
-          .measured_absolute_position = 0.0;
-      turret->potentiometer_offset = 0.0;
+          .measured_absolute_position = 0.051;
+      turret->potentiometer_offset = -7.932;
       turret->subsystem_params.zeroing_constants.measured_absolute_position =
-          0.0;
-      flipper_arm_left->potentiometer_offset = 0.0;
-      flipper_arm_right->potentiometer_offset = 0.0;
+          1.166;
+      flipper_arm_left->potentiometer_offset = -6.40;
+      flipper_arm_right->potentiometer_offset = 5.56;
 
       catapult_params->zeroing_constants.measured_absolute_position = 0.0;
-      catapult->potentiometer_offset = 0.0;
+      catapult->potentiometer_offset = -2.033;
 
       ball_color->reference_red = {0, 0, 1, 1};
       ball_color->reference_blue = {0, 0, 1, 1};
@@ -310,8 +310,10 @@ Values MakeValues(uint16_t team) {
           .upper = 2.9         // Front Soft
       };
       turret_params->range = *turret_range;
-      flipper_arm_left->potentiometer_offset = -4.39536583413615 - 0.108401297910291;
-      flipper_arm_right->potentiometer_offset = 4.36264091401229 + 0.175896445665755;
+      flipper_arm_left->potentiometer_offset =
+          -4.39536583413615 - 0.108401297910291;
+      flipper_arm_right->potentiometer_offset =
+          4.36264091401229 + 0.175896445665755;
 
       catapult_params->zeroing_constants.measured_absolute_position =
           1.62909518684227;
