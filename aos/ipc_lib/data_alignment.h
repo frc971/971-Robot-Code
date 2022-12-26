e8d44ffdb4d984350dc517e2af87913246422da9
Austin Schuh <austin.linux@gmail.com>
2022-12-25 22:20:02 -0800

    Increase alignment to 128 bytes
    Rockpi's DMA engine requires 128 byte alignment.  Since this is the new
    high point, up it.
    
    Change-Id: Iac4f7c0a5e6e98c7f3e5d21c1cb7d03483a511df
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- aos/ipc_lib/data_alignment.h
+++ aos/ipc_lib/data_alignment.h
@@ -9,11 +9,13 @@ namespace aos {
 // alignment for their end. Flatbuffers aligns from the end, so this is what
 // matters.
 //
-// 64 is a reasonable choice for now:
+// 128 is a reasonable choice for now:
 //   Cortex-A72 (Raspberry Pi 4) and Cortex-A53 (Xavier AGX) both have 64 byte
 //   cache lines.
 //   V4L2 requires 64 byte alignment for USERPTR buffers.
-static constexpr size_t kChannelDataAlignment = 64;
+//
+//   rockpi v4l2 requires 128 byte alignment for USERPTR buffers.
+static constexpr size_t kChannelDataAlignment = 128;
 
 template <typename T>
 inline void CheckChannelDataAlignment(T *data, size_t size) {
