2917b0cd093754dfaa94fe7a543cd274961d2f2f
Austin Schuh <austin.linux@gmail.com>
2023-01-03 21:18:31 -0800

    Make the deploy_kernel.sh rockpi script take a path
    It is handy to not read ./linux-kernel-${KERNEL_VERSION}.tar.xz when
    scripting.  Take an optional argument which is the path.
    
    Change-Id: I1ee9da0e5f1e558d2d2247def44dca2828a1864f
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- frc971/rockpi/contents/root/bin/deploy_kernel.sh
+++ frc971/rockpi/contents/root/bin/deploy_kernel.sh
@@ -3,8 +3,11 @@
 set -ex
 
 KERNEL_VERSION=6.0.8-rt14-rockpi4b
+TAR=${1-./linux-kernel-${KERNEL_VERSION}.tar.xz}
+
+KERNEL_VERSION=$(echo "${TAR}" | sed 's/.*linux-kernel-\(.*\).tar.xz/\1/')
 
 rm -rf /boot/dtbs/${KERNEL_VERSION}/
 rm -rf /lib/modules/${KERNEL_VERSION}/
-tar --owner=0 --group=0 --no-same-owner --no-same-permissions -xvf ./linux-kernel-${KERNEL_VERSION}.tar.xz -C /boot/ --strip-components=2 ./boot/
-tar --strip-components=3 -xvf linux-kernel-${KERNEL_VERSION}.tar.xz -C /lib/modules/ ./lib/modules
+tar --owner=0 --group=0 --no-same-owner --no-same-permissions -xvf ${TAR} -C /boot/ --strip-components=2 ./boot/
+tar --strip-components=3 -xvf ${TAR} -C /lib/modules/ ./lib/modules
