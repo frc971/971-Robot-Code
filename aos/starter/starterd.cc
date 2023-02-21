09ec0075dc086751fc50000ee538a97d756f5a8f
Austin Schuh <austin.linux@gmail.com>
2023-02-21 14:17:02 -0800

    Add --purge_shm_base to aos_starter
    This removes everything in /dev/shm/aos before doing anything.  This
    gives us options to use SUID on the roboRIO to become admin and then
    clean up the old shm_base as admin.  Otherwise, we were getting into a
    crash loop.
    
    Change-Id: Ia880af19a63cd30043fe92df3bdb2d13fd08dfb8
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- aos/starter/starterd.cc
+++ aos/starter/starterd.cc
@@ -2,15 +2,25 @@
 #include <sys/types.h>
 
 #include "aos/init.h"
+#include "aos/starter/starterd_lib.h"
+#include "aos/util/file.h"
 #include "gflags/gflags.h"
-#include "starterd_lib.h"
 
 DEFINE_string(config, "aos_config.json", "File path of aos configuration");
 DEFINE_string(user, "",
               "Starter runs as though this user ran a SUID binary if set.");
 
+DECLARE_string(shm_base);
+DEFINE_bool(purge_shm_base, false,
+            "If true, delete everything in --shm_base before starting.");
+
 int main(int argc, char **argv) {
   aos::InitGoogle(&argc, &argv);
+
+  if (FLAGS_purge_shm_base) {
+    aos::util::UnlinkRecursive(FLAGS_shm_base);
+  }
+
   if (!FLAGS_user.empty()) {
     uid_t uid;
     uid_t gid;
