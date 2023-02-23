65d2f7287683c756b17da3b0b605483ecf3af2fa
James Kuszmaul <jabukuszmaul@gmail.com>
2023-02-23 13:03:43 -0800

    Stop populating "imu" in /etc/hosts
    We now identify the imu pi in the AOS config as "pi5" rather than as
    "imu". Rather than updating the entry in the /etc/hosts, just remove it.
    
    Change-Id: Ie66b6e0b92dffdcabfbf8fae1d5da1ba82b14d09
    Signed-off-by: James Kuszmaul <jabukuszmaul@gmail.com>

--- frc971/rockpi/contents/root/bin/change_hostname.sh
+++ frc971/rockpi/contents/root/bin/change_hostname.sh
@@ -40,23 +40,10 @@ else
   done
 fi
 
-# Put corret team number in roborio's address, or add it if missing
+# Put correct team number in roborio's address, or add it if missing
 if grep '^10\.[0-9]*\.[0-9]*\.2\s*roborio$' /etc/hosts >/dev/null;
 then
   sed -i "s/^10\.[0-9]*\.[0-9]*\(\.2\s*roborio\)$/${IP_BASE}\1/" /etc/hosts
 else
   echo -e "${IP_BASE}.2\troborio" >> /etc/hosts
-fi
-
-# Put corret team number in imu's address, or add it if missing
-if grep '^10\.[0-9]*\.[0-9]*\.105\s.*\s*imu$' /etc/hosts >/dev/null;
-then
-  sed -i "s/^10\.[0-9]*\.[0-9]*\(\.[0-9]*\s*pi-\)[0-9]*\(-[0-9] pi5 imu\)$/${IP_BASE}\1${TEAM_NUMBER}\2/" /etc/hosts
-else
-  if grep '^10\.[0-9]*\.[0-9]*\.105\s*pi-[0-9]*-[0-9]*\s*pi5$' /etc/hosts
-  then
-    sed -i "s/^10\.[0-9]*\.[0-9]*\(\.[0-9]*\s*pi-\)[0-9]*\(-[0-9] pi5\)$/${IP_BASE}\1${TEAM_NUMBER}\2 imu/" /etc/hosts
-  else
-    echo -e "${IP_BASE}.105\tpi-${TEAM_NUMBER}-5 pi5 imu" >> /etc/hosts
-  fi
 fi
