b80e2406b542822feac7e83393af052dc8b611b6
James Kuszmaul <jabukuszmaul@gmail.com>
2023-03-10 22:25:15 -0800

    Fix field dimensions for 2023 debug webpage
    Change-Id: I1235d6c6e0cf13a1bc9e8f5eed33de820a544065
    Signed-off-by: James Kuszmaul <jabukuszmaul@gmail.com>

--- y2023/www/constants.ts
+++ y2023/www/constants.ts
@@ -2,6 +2,7 @@
 export const IN_TO_M = 0.0254;
 export const FT_TO_M = 0.3048;
 // Dimensions of the field in meters
-export const FIELD_WIDTH = 26 * FT_TO_M + 11.25 * IN_TO_M;
-export const FIELD_LENGTH = 52 * FT_TO_M + 5.25 * IN_TO_M;
+// Numbers are slightly hand-tuned to match the PNG that we are using.
+export const FIELD_WIDTH = 26 * FT_TO_M + 7.25 * IN_TO_M;
+export const FIELD_LENGTH = 54 * FT_TO_M + 5.25 * IN_TO_M;
 
