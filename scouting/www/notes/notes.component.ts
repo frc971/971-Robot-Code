7e19827fd046773948226f7832e3371c0b5fa484
Filip Kujawa <filip.j.kujawa@gmail.com>
2023-03-15 21:01:18 -0700

    Scouting: Add match number to note scouting entry
    Signed-off-by: Filip Kujawa <filip.j.kujawa@gmail.com>
    Change-Id: Ib389b76d137231ef639a7a67df91d545741cbd1d

--- scouting/www/notes/notes.component.ts
+++ scouting/www/notes/notes.component.ts
@@ -111,7 +111,7 @@ export class Notes {
   setTeamNumber() {
     let data: Input = {
       teamNumber: this.teamNumberSelection,
-      notesData: 'Auto: \nTeleop: \nEngame: ',
+      notesData: 'Match: \nAuto: \nTeleop: \nEngame: ',
       keywordsData: {
         goodDriving: false,
         badDriving: false,
