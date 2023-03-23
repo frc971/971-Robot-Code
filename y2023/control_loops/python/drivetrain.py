dd057bc2403fe5029008b78678f69a10a70db2f0
Austin Schuh <austin.linux@gmail.com>
2023-03-22 20:26:44 -0700

    Tune drivetrain controller
    Sigh, no IMU, and the gear ratio was wrong.
    
    Change-Id: Ica4f037c2307daae891b45a651592400b57bdf1b
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- y2023/control_loops/python/drivetrain.py
+++ y2023/control_loops/python/drivetrain.py
@@ -14,17 +14,17 @@ gflags.DEFINE_bool('plot', False, 'If true, plot the loop response.')
 
 kDrivetrain = drivetrain.DrivetrainParams(
     J=6.5,
-    mass=58.0,
+    mass=68.0,
     # TODO(austin): Measure radius a bit better.
     robot_radius=0.39,
     wheel_radius=2.5 * 0.0254,
     motor_type=control_loop.Falcon(),
     num_motors=3,
-    G=(14.0 / 54.0) * (22.0 / 56.0),
+    G=(14.0 / 52.0) * (26.0 / 58.0),
     q_pos=0.24,
     q_vel=2.5,
-    efficiency=0.75,
-    has_imu=True,
+    efficiency=0.92,
+    has_imu=False,
     force=True,
     kf_q_voltage=1.0,
     controller_poles=[0.82, 0.82])
