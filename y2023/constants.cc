6c90f053d890b12bee1e9c2d4fab67c31e73d3d2
James Kuszmaul <jabukuszmaul@gmail.com>
2023-04-08 17:54:47 -0700

    Update calibration on both robots.
    Change-Id: I3ae7527db0215a7977f199b02dc4a26a65a2d3a0
    Signed-off-by: James Kuszmaul <jabukuszmaul@gmail.com>

--- y2023/constants.cc
+++ y2023/constants.cc
@@ -83,24 +83,24 @@ Values MakeValues(uint16_t team) {
       break;
 
     case kCompTeamNumber:
-      arm_proximal->zeroing.measured_absolute_position = 0.149723235965925;
+      arm_proximal->zeroing.measured_absolute_position = 0.152273750996612;
       arm_proximal->potentiometer_offset =
           0.931355973012855 + 8.6743197253382 - 0.101200335326309 -
           0.0820901660993467 - 0.0703733798337964 - 0.0294645384848748 -
-          0.577156175549626;
+          0.577156175549626 - 0.000944609125286267;
 
-      arm_distal->zeroing.measured_absolute_position = 0.190748983191916;
+      arm_distal->zeroing.measured_absolute_position = 0.201204843551682;
       arm_distal->potentiometer_offset =
           0.436664933370656 + 0.49457213779426 + 6.78213223139724 -
           0.0220711555235029 - 0.0162945074111813 + 0.00630344935527365 -
           0.0164398318919943 - 0.145833494945215 + 0.234878799868491 +
           0.125924230298394 + 0.147136306208754 - 0.69167546169753 -
-          0.308761538844425 + 0.610386472488493;
+          0.308761538844425 + 0.610386472488493 + 0.08384162885249 + 0.0262274735196811;
 
       arm_distal->zeroing.one_revolution_distance =
           M_PI * 2.0 * constants::Values::kDistalEncoderRatio();
 
-      roll_joint->zeroing.measured_absolute_position = 0.619108755444215;
+      roll_joint->zeroing.measured_absolute_position = 0.419144048980465;
       roll_joint->potentiometer_offset =
           -(3.87038557084874 - 0.0241774522172967 + 0.0711345168020632 -
             0.866186131631967 - 0.0256788357596952 + 0.18101759154572017 -
@@ -108,37 +108,39 @@ Values MakeValues(uint16_t team) {
             0.5935210745062 + 0.166256655718334 - 0.12591438680483 +
             0.11972765117321 - 0.318724743041507) +
           0.0201047336425017 - 1.0173426655158 - 0.186085272847293 -
-          0.0317706563397807;
+          0.0317706563397807 - 2.6357823523782 + 0.871932806570122;
 
       wrist->subsystem_params.zeroing_constants.measured_absolute_position =
-          2.29414865465015;
+          2.29036834725319;
 
       break;
 
     case kPracticeTeamNumber:
-      arm_proximal->zeroing.measured_absolute_position = 0.24572544970387;
+      arm_proximal->zeroing.measured_absolute_position = 0.140348044909775;
       arm_proximal->potentiometer_offset =
           10.5178592988554 + 0.0944609125285876 - 0.00826532984625095 +
-          0.167359305216504;
+          0.167359305216504 + 0.135144500925909;
 
-      arm_distal->zeroing.measured_absolute_position = 0.0698375027814462;
+      arm_distal->zeroing.measured_absolute_position = 0.860219239796068;
       arm_distal->potentiometer_offset =
           7.673132586937 - 0.0799284644472573 - 0.0323574039310657 +
           0.0143810684138064 + 0.00945555248207735 + 0.452446388633863 +
-          0.0194863477007102;
+          0.0194863477007102  + 0.235993332670562 + 0.00138417783482921;
 
       arm_distal->zeroing.one_revolution_distance =
           M_PI * 2.0 * constants::Values::kDistalEncoderRatio() *
-          3.11964893168338 / 3.148;
+          //3.11964893168338 / 3.148;
+          (3.12725165289659 + 0.002) / 3.1485739705977704;
 
-      roll_joint->zeroing.measured_absolute_position = 0.0722321007692069;
+      roll_joint->zeroing.measured_absolute_position = 1.78072413296938;
       roll_joint->potentiometer_offset =
           0.624713611895747 + 3.10458504917251 - 0.0966407797407789 +
           0.0257708772364788 - 0.0395076737853459 - 6.87914956118006 -
-          0.097581301615046 + 3.3424421683095 - 3.97605190912604;
+          0.097581301615046 + 3.3424421683095 - 3.97605190912604 +
+          0.709274294168941;
 
       wrist->subsystem_params.zeroing_constants.measured_absolute_position =
-          1.04410369068834;
+          6.05398282057911;
 
       break;
 
