edebe941c2662421013f66e3b2fb886538e14cd7
Austin Schuh <austin.linux@gmail.com>
2023-08-30 20:51:31 -0700

    Remove unused macros in aos/macros.h
    Change-Id: Ifed889de2ab4079cc27f6229a3e19eb31042d53c
    Signed-off-by: Austin Schuh <austin.linux@gmail.com>

--- aos/macros.h
+++ aos/macros.h
@@ -9,33 +9,13 @@
   TypeName(const TypeName &) = delete;     \
   void operator=(const TypeName &) = delete
 
-// A macro to wrap arguments to macros that contain commas.
-// Useful for DISALLOW_COPY_AND_ASSIGNing templated types with multiple template
-// arguments.
-#define MACRO_ARG(...) __VA_ARGS__
-// Double-wraps macro arguments.
-// Necessary to use commas in gtest predicate arguments.
-#define MACRO_DARG(...) (__VA_ARGS__)
-
-#ifdef __GNUC__
-#define UNUSED_VARIABLE __attribute__((unused))
-#else
-#define UNUSED_VARIABLE
-#endif
-
 #define AOS_STRINGIFY(x) AOS_TO_STRING(x)
 #define AOS_TO_STRING(x) #x
 
-#ifdef __VXWORKS__
-// We're using ancient glibc, so sticking to just what the syscall can handle is
-// probably safer.
-#define GOOD_PRINTF_FORMAT_TYPE printf
-#else
 #ifdef __clang__
 #define GOOD_PRINTF_FORMAT_TYPE __printf__
 #else
 #define GOOD_PRINTF_FORMAT_TYPE gnu_printf
-#endif
 #endif
 
 #endif  // _AOS_MACROS_H_
