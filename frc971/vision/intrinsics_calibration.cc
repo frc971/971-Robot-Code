b974cca06710ea720f382f610026fea824bd32a5
Jim Ostrowski <yimmy13@gmail.com>
2024-01-28 15:07:50 -0800

    Moving camera calib files to use camera#
    Modifying intrinsics_calibration to handle and use this new format
    
    Changing numbering to match /dev/video#, so that camera on /dev/video0
    is publishing on /camera0 for example
    
    Added a utility function to get camera number from channel, and a test to
    go with it
    
    Cleaned up some of the logging to use percentages instead of fractions /
    decimals
    
    Change-Id: I764d59ca7d9089a37d010272879ce55aae5dbd95
    Signed-off-by: Jim Ostrowski <yimmy13@gmail.com>

--- frc971/vision/intrinsics_calibration.cc
+++ frc971/vision/intrinsics_calibration.cc
@@ -13,15 +13,17 @@
 #include "aos/util/file.h"
 #include "frc971/vision/intrinsics_calibration_lib.h"
 
-DEFINE_string(calibration_folder, ".", "Folder to place calibration files.");
-DEFINE_string(camera_id, "", "Camera ID in format YY-NN-- year and number.");
-DEFINE_string(config, "aos_config.json", "Path to the config file to use.");
-DEFINE_bool(display_undistorted, false,
-            "If true, display the undistorted image.");
-DEFINE_string(pi, "", "Pi name to calibrate.");
+// TODO: Would be nice to remove this, but it depends on year-by-year Constants
 DEFINE_string(base_intrinsics, "",
               "Intrinsics to use for estimating board pose prior to solving "
               "for the new intrinsics.");
+DEFINE_string(calibration_folder, ".", "Folder to place calibration files.");
+DEFINE_string(camera_id, "", "Camera ID in format YY-NN-- year and number.");
+DEFINE_string(channel, "/camera", "Camera channel to use");
+DEFINE_string(config, "aos_config.json", "Path to the config file to use.");
+DEFINE_string(cpu_name, "", "Pi/Orin name to calibrate.");
+DEFINE_bool(display_undistorted, false,
+            "If true, display the undistorted image.");
 
 namespace frc971::vision {
 namespace {
@@ -32,7 +34,7 @@ void Main() {
 
   aos::ShmEventLoop event_loop(&config.message());
 
-  std::string hostname = FLAGS_pi;
+  std::string hostname = FLAGS_cpu_name;
   if (hostname == "") {
     hostname = aos::network::GetHostname();
     LOG(INFO) << "Using pi/orin name from hostname as " << hostname;
@@ -41,9 +43,10 @@ void Main() {
       << "Need a base intrinsics json to use to auto-capture images when the "
          "camera moves.";
   std::unique_ptr<aos::ExitHandle> exit_handle = event_loop.MakeExitHandle();
-  IntrinsicsCalibration extractor(
-      &event_loop, hostname, FLAGS_camera_id, FLAGS_base_intrinsics,
-      FLAGS_display_undistorted, FLAGS_calibration_folder, exit_handle.get());
+  IntrinsicsCalibration extractor(&event_loop, hostname, FLAGS_channel,
+                                  FLAGS_camera_id, FLAGS_base_intrinsics,
+                                  FLAGS_display_undistorted,
+                                  FLAGS_calibration_folder, exit_handle.get());
 
   event_loop.Run();
 
