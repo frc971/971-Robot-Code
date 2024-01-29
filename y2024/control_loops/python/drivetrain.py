7431ab22c2740f7d3c4385b20d5f04bafd5f0079
Filip Kujawa <filip.j.kujawa@gmail.com>
2024-01-28 17:21:57 -0800

    Update y2024 drivetrain gear ratio and wheel radius
    Signed-off-by: Filip Kujawa <filip.j.kujawa@gmail.com>
    Change-Id: Id06e9cdeb05b1dae905fca7dc01412b434767105

--- y2024/control_loops/python/drivetrain.py
+++ y2024/control_loops/python/drivetrain.py
@@ -10,24 +10,25 @@ import glog
 
 FLAGS = gflags.FLAGS
 
-gflags.DEFINE_bool('plot', False, 'If true, plot the loop response.')
+gflags.DEFINE_bool("plot", False, "If true, plot the loop response.")
 
 kDrivetrain = drivetrain.DrivetrainParams(
     J=6.5,
     mass=68.0,
     # TODO(austin): Measure radius a bit better.
     robot_radius=0.39,
-    wheel_radius=2.5 * 0.0254,
+    wheel_radius=2 * 0.0254,
     motor_type=control_loop.KrakenFOC(),
-    num_motors=3,
-    G=(14.0 / 52.0) * (26.0 / 58.0),
+    num_motors=2,
+    G=(14.0 / 52.0) * (36.0 / 56.0),
     q_pos=0.24,
     q_vel=2.5,
     efficiency=0.92,
     has_imu=False,
     force=True,
     kf_q_voltage=1.0,
-    controller_poles=[0.82, 0.82])
+    controller_poles=[0.82, 0.82],
+)
 
 
 def main(argv):
@@ -40,8 +41,8 @@ def main(argv):
         print("Expected .h, .cc, and .json filenames")
     else:
         # Write the generated constants out to a file.
-        drivetrain.WriteDrivetrain(argv[1:4], argv[4:7], 'y2024', kDrivetrain)
+        drivetrain.WriteDrivetrain(argv[1:4], argv[4:7], "y2024", kDrivetrain)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     sys.exit(main(sys.argv))
