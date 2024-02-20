fab83ad2233ca2c2e4e436a5f2ddae23f3b243ea
Maxwell Henderson <mxwhenderson@gmail.com>
2024-02-20 15:32:36 -0800

    Change the usage constants fetcher in drivetrain
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>
    Change-Id: I8963a27272784d46892a0af73b2050010e9acbd4

--- y2024/control_loops/drivetrain/drivetrain_main.cc
+++ y2024/control_loops/drivetrain/drivetrain_main.cc
@@ -18,14 +18,15 @@ int main(int argc, char **argv) {
   frc971::constants::WaitForConstants<y2024::Constants>(&config.message());
 
   aos::ShmEventLoop event_loop(&config.message());
+  const auto drivetrain_config =
+      ::y2024::control_loops::drivetrain::GetDrivetrainConfig(&event_loop);
+
   std::unique_ptr<::frc971::control_loops::drivetrain::PuppetLocalizer>
       localizer = std::make_unique<
           ::frc971::control_loops::drivetrain::PuppetLocalizer>(
-          &event_loop,
-          ::y2024::control_loops::drivetrain::GetDrivetrainConfig(&event_loop));
+          &event_loop, drivetrain_config);
   std::unique_ptr<DrivetrainLoop> drivetrain = std::make_unique<DrivetrainLoop>(
-      y2024::control_loops::drivetrain::GetDrivetrainConfig(&event_loop),
-      &event_loop, localizer.get());
+      drivetrain_config, &event_loop, localizer.get());
 
   event_loop.Run();
 
