736776b5a9ef2367e8cf5937e38f08606a4c0016
Maxwell Henderson <mxwhenderson@gmail.com>
2025-01-06 14:54:26 -0800

    Update swerve_control_loops.* to do basic linear controller
    Change-Id: Icc63cb37ec0c2a0e8086f11fa67c4639a447ff4b
    Signed-off-by: James Kuszmaul <jabukuszmaul+collab@gmail.com>

--- frc971/control_loops/swerve/swerve_control_loops.cc
+++ frc971/control_loops/swerve/swerve_control_loops.cc
@@ -1,142 +1,282 @@
 #include "frc971/control_loops/swerve/swerve_control_loops.h"
 
+#include "aos/commonmath.h"
+
+ABSL_FLAG(int, swerve_priority, 20, "");
+ABSL_FLAG(int, swerve_skip_iters, 1,
+          "Set to the number of iterations to skip + 1.");
+ABSL_FLAG(bool, drive_heading, true, "");
+
 namespace frc971::control_loops::swerve {
 
 SwerveControlLoops::SwerveControlLoops(
     ::aos::EventLoop *event_loop,
     const frc971::control_loops::
-        StaticZeroingSingleDOFProfiledSubsystemCommonParams *rotation_params,
-    const SwerveZeroing *zeroing_params, const ::std::string &name)
+        StaticZeroingSingleDOFProfiledSubsystemCommonParams *,
+    const SwerveZeroing *zeroing_params,
+    const NaiveEstimator::Parameters &params,
+    const LinearVelocityController::ControllerWeights &lvc_weights,
+    const ::std::string &name)
     : frc971::controls::ControlLoop<Goal, Position, StatusStatic, OutputStatic>(
           event_loop, name),
-      front_left_(rotation_params, zeroing_params->front_left()),
-      front_right_(rotation_params, zeroing_params->front_right()),
-      back_left_(rotation_params, zeroing_params->back_left()),
-      back_right_(rotation_params, zeroing_params->back_right()) {}
+      can_position_fetcher_(event_loop->MakeFetcher<CanPosition>(name)),
+      gyro_fetcher_(event_loop->TryMakeFetcher<::frc971::sensors::GyroReading>(
+          "/drivetrain")),
+      imu_fetcher_(
+          event_loop->TryMakeFetcher<::frc971::IMUValuesBatch>("/localizer")),
+      naive_estimator_(zeroing_params, params),
+      velocity_controller_(
+          LinearVelocityController::MakeParameters(lvc_weights, params),
+          params),
+      inverse_kinematics_(params),
+      velocity_ekf_(params) {
+  if (absl::GetFlag(FLAGS_swerve_priority) > 0) {
+    event_loop->SetRuntimeRealtimePriority(
+        absl::GetFlag(FLAGS_swerve_priority));
+  }
+}
 
 void SwerveControlLoops::RunIteration(
     const Goal *goal, const Position *position,
     aos::Sender<OutputStatic>::StaticBuilder *output_builder,
     aos::Sender<StatusStatic>::StaticBuilder *status_builder) {
-  if (WasReset()) {
-    front_left_.Reset();
-    front_right_.Reset();
-    back_left_.Reset();
-    back_right_.Reset();
+  ++iteration_counter_;
+  if (iteration_counter_ % absl::GetFlag(FLAGS_swerve_skip_iters) != 0) {
+    return;
   }
-  aos::FlatbufferFixedAllocatorArray<
-      frc971::control_loops::StaticZeroingSingleDOFProfiledSubsystemGoal, 512>
-      front_left_goal_buffer, front_right_goal_buffer, back_left_goal_buffer,
-      back_right_goal_buffer;
+  const aos::monotonic_clock::time_point profiling_start_time =
+      aos::monotonic_clock::now();
+  const aos::monotonic_clock::time_point now =
+      event_loop()->context().monotonic_event_time;
 
-  front_left_goal_buffer.Finish(
-      frc971::control_loops::CreateStaticZeroingSingleDOFProfiledSubsystemGoal(
-          *front_left_goal_buffer.fbb(),
-          (goal != nullptr && goal->has_front_left_goal())
-              ? goal->front_left_goal()->rotation_angle()
-              : 0.0));
-  front_right_goal_buffer.Finish(
-      frc971::control_loops::CreateStaticZeroingSingleDOFProfiledSubsystemGoal(
-          *front_right_goal_buffer.fbb(),
-          (goal != nullptr && goal->has_front_right_goal())
-              ? goal->front_right_goal()->rotation_angle()
-              : 0.0));
-  back_left_goal_buffer.Finish(
-      frc971::control_loops::CreateStaticZeroingSingleDOFProfiledSubsystemGoal(
-          *back_left_goal_buffer.fbb(),
-          (goal != nullptr && goal->has_back_left_goal())
-              ? goal->back_left_goal()->rotation_angle()
-              : 0.0));
-  back_right_goal_buffer.Finish(
-      frc971::control_loops::CreateStaticZeroingSingleDOFProfiledSubsystemGoal(
-          *back_right_goal_buffer.fbb(),
-          (goal != nullptr && goal->has_back_right_goal())
-              ? goal->back_right_goal()->rotation_angle()
-              : 0.0));
-  const bool disabled = front_left_.Correct(
-      &front_left_goal_buffer.message(),
-      position->front_left()->rotation_position(), output_builder == nullptr);
-  front_right_.Correct(&front_right_goal_buffer.message(),
-                       position->front_right()->rotation_position(),
-                       output_builder == nullptr);
-  back_left_.Correct(&back_left_goal_buffer.message(),
-                     position->back_left()->rotation_position(),
-                     output_builder == nullptr);
-  back_right_.Correct(&back_right_goal_buffer.message(),
-                      position->back_right()->rotation_position(),
-                      output_builder == nullptr);
+  can_position_fetcher_.Fetch();
 
-  const double front_left_voltage = front_left_.UpdateController(disabled);
-  front_left_.UpdateObserver(front_left_voltage);
-  const double front_right_voltage = front_right_.UpdateController(disabled);
-  front_right_.UpdateObserver(front_right_voltage);
-  const double back_left_voltage = back_left_.UpdateController(disabled);
-  back_left_.UpdateObserver(back_left_voltage);
-  const double back_right_voltage = back_right_.UpdateController(disabled);
-  back_right_.UpdateObserver(back_right_voltage);
-  (void)goal, (void)position;
-  aos::FlatbufferFixedAllocatorArray<
-      frc971::control_loops::AbsoluteEncoderProfiledJointStatus, 512>
-      front_left_status_buffer, front_right_status_buffer,
-      back_left_status_buffer, back_right_status_buffer;
-  front_left_status_buffer.Finish(
-      front_left_.MakeStatus(front_left_status_buffer.fbb()));
-  front_right_status_buffer.Finish(
-      front_right_.MakeStatus(front_right_status_buffer.fbb()));
-  back_left_status_buffer.Finish(
-      back_left_.MakeStatus(back_left_status_buffer.fbb()));
-  back_right_status_buffer.Finish(
-      back_right_.MakeStatus(back_right_status_buffer.fbb()));
+  if (gyro_fetcher_.valid()) {
+    gyro_fetcher_.Fetch();
+  }
+  if (imu_fetcher_.valid()) {
+    while (imu_fetcher_.FetchNext()) {
+      for (const IMUValues *values : *imu_fetcher_->readings()) {
+        imu_zeroer_.InsertMeasurement(*values);
+      }
+    }
+    imu_zeroer_.ProcessMeasurements();
+  }
+
+  std::optional<NaiveEstimator::State> current_state;
+  std::optional<double> gyro_rate;
+  if (gyro_fetcher_.valid() && gyro_fetcher_.get() != nullptr) {
+    gyro_rate = gyro_fetcher_->velocity();
+    if (!yaw_gyro_zero_.has_value()) {
+      yaw_gyro_zeroer_.AddData(gyro_rate.value());
+      // Maximum variation to allow in the gyro when zeroing.
+      constexpr double kMaxYawGyroZeroingRange = 0.15;
+      if (yaw_gyro_zeroer_.full() &&
+          yaw_gyro_zeroer_.GetRange() < kMaxYawGyroZeroingRange) {
+        yaw_gyro_zero_ = yaw_gyro_zeroer_.GetAverage()(0);
+        VLOG(1) << "Zeroed to " << *yaw_gyro_zero_ << " Range "
+                << yaw_gyro_zeroer_.GetRange();
+      }
+    }
+    if (yaw_gyro_zero_.has_value()) {
+      gyro_rate = gyro_rate.value() - yaw_gyro_zero_.value();
+    } else {
+      gyro_rate = 0.0;
+    }
+  }
+
+  std::optional<Eigen::Vector3d> zeroed_gyro = imu_zeroer_.ZeroedGyro();
+  if (zeroed_gyro.has_value()) {
+    gyro_rate = zeroed_gyro.value().z();
+  }
+
+  if (gyro_rate.has_value() && can_position_fetcher_.get() != nullptr) {
+    current_state = naive_estimator_.Update(
+        now, position, can_position_fetcher_.get(), gyro_rate.value());
+    if (!ekf_initialized_) {
+      velocity_ekf_.Initialize(now, current_state.value());
+      ekf_initialized_ = true;
+    }
+    velocity_ekf_.Update(
+        now,
+        Eigen::Matrix<Scalar, 4, 1>{{current_state.value()(States::kThetas0)},
+                                    {current_state.value()(States::kThetas1)},
+                                    {current_state.value()(States::kThetas2)},
+                                    {current_state.value()(States::kThetas3)}},
+        {
+            aos::monotonic_clock::time_point{
+                std::chrono::nanoseconds(can_position_fetcher_->front_left()
+                                             ->translation()
+                                             ->timestamp())},
+            aos::monotonic_clock::time_point{
+                std::chrono::nanoseconds(can_position_fetcher_->front_right()
+                                             ->translation()
+                                             ->timestamp())},
+            aos::monotonic_clock::time_point{
+                std::chrono::nanoseconds(can_position_fetcher_->back_left()
+                                             ->translation()
+                                             ->timestamp())},
+            aos::monotonic_clock::time_point{
+                std::chrono::nanoseconds(can_position_fetcher_->back_right()
+                                             ->translation()
+                                             ->timestamp())},
+        },
+        Eigen::Matrix<Scalar, 4, 1>{
+            {can_position_fetcher_->front_left()->translation()->position()},
+            {can_position_fetcher_->front_right()->translation()->position()},
+            {can_position_fetcher_->back_left()->translation()->position()},
+            {can_position_fetcher_->back_right()->translation()->position()}},
+        gyro_rate.value(), U_,
+        (status_builder != nullptr) ? status_builder->get()->add_velocity_ekf()
+                                    : nullptr);
+  }
+
+  const aos::monotonic_clock::time_point estimation_done =
+      aos::monotonic_clock::now();
+  U_.setZero();
+  std::optional<LinearVelocityController::ControllerResult> controller_result;
+  if (goal != nullptr && current_state.has_value()) {
+    CHECK_NE(goal->has_linear_velocity_goal(), goal->has_joystick_goal());
+    if (goal->has_linear_velocity_goal()) {
+      NaiveEstimator::State goal_state =
+          ToEigenOrDie<NaiveEstimator::States::kNumVelocityStates, 1>(
+              *goal->linear_velocity_goal()->state())
+              .cast<Scalar>();
+      controller_result = velocity_controller_.RunRawController(
+          current_state.value(), goal_state,
+          ToEigenOrDie<8, 1>(*goal->linear_velocity_goal()->input())
+              .cast<Scalar>());
+    } else if (goal->has_joystick_goal()) {
+      if (!desired_heading_.has_value()) {
+        desired_heading_ = JoystickHeadingGoal{
+            .heading = current_state.value()(NaiveEstimator::States::kTheta),
+            .last_time = now};
+      }
+      NaiveEstimator::State kinematics_state = current_state.value();
+      kinematics_state(NaiveEstimator::States::kVx) =
+          goal->joystick_goal()->vx();
+      kinematics_state(NaiveEstimator::States::kVy) =
+          goal->joystick_goal()->vy();
+      const Scalar goal_omega = goal->joystick_goal()->omega();
+      const Scalar current_omega =
+          kinematics_state(NaiveEstimator::States::kOmega);
+      const Scalar current_theta =
+          kinematics_state(NaiveEstimator::States::kTheta);
+      kinematics_state(NaiveEstimator::States::kOmega) = goal_omega;
+      const Scalar omega_capped =
+          (goal_omega > 0)
+              ? std::min(goal_omega, std::max<Scalar>(0.0, current_omega))
+              : std::max(goal_omega, std::min<Scalar>(0.0, current_omega));
+      desired_heading_.value().heading +=
+          omega_capped * aos::time::DurationInSeconds(
+                             now - desired_heading_.value().last_time);
+      desired_heading_.value().heading =
+          aos::Clip(desired_heading_.value().heading, current_theta - 0.1,
+                    current_theta + 0.1);
+      desired_heading_.value().last_time = now;
+      if (absl::GetFlag(FLAGS_drive_heading)) {
+        kinematics_state(NaiveEstimator::States::kTheta) =
+            desired_heading_.value().heading;
+      }
+
+      controller_result = velocity_controller_.RunRawController(
+          current_state.value(), inverse_kinematics_.Solve(kinematics_state),
+          Eigen::Matrix<Scalar, 8, 1>::Zero());
+    } else {
+      LOG(FATAL) << "Unreachable";
+    }
+    U_ = controller_result->U.cast<float>();
+  }
+
+  const aos::monotonic_clock::time_point controller_done =
+      aos::monotonic_clock::now();
+
+  constexpr Scalar kMaxDriveCurrent = 100.0;
+
+  const Scalar reference_drive_current = std::max<Scalar>(
+      kMaxDriveCurrent,
+      std::max(
+          {std::abs(U_(InputStates::kIs0)), std::abs(U_(InputStates::kIs1)),
+           std::abs(U_(InputStates::kIs2)), std::abs(U_(InputStates::kIs3))}));
+  const Scalar drive_current_scalar =
+      kMaxDriveCurrent / reference_drive_current;
 
   if (output_builder != nullptr) {
     OutputStatic *output = output_builder->get();
 
-    auto front_left_output = output->add_front_left_output();
-    front_left_output->set_rotation_current(front_left_voltage);
-    front_left_output->set_translation_current(
-        goal ? goal->front_left_goal()->translation_current() : 0.0);
-
-    auto front_right_output = output->add_front_right_output();
-    front_right_output->set_rotation_current(front_right_voltage);
-    front_right_output->set_translation_current(
-        goal ? goal->front_right_goal()->translation_current() : 0.0);
-
-    auto back_left_output = output->add_back_left_output();
-    back_left_output->set_rotation_current(back_left_voltage);
-    back_left_output->set_translation_current(
-        goal ? goal->back_left_goal()->translation_current() : 0.0);
-
-    auto back_right_output = output->add_back_right_output();
-    back_right_output->set_rotation_current(back_right_voltage);
-    back_right_output->set_translation_current(
-        goal ? goal->back_right_goal()->translation_current() : 0.0);
+    {
+      auto module_output = output->add_front_left_output();
+      module_output->set_rotation_current(U_(InputStates::kIs0) *
+                                          drive_current_scalar);
+      module_output->set_translation_current(U_(InputStates::kId0));
+    }
+    {
+      auto module_output = output->add_front_right_output();
+      module_output->set_rotation_current(U_(InputStates::kIs1) *
+                                          drive_current_scalar);
+      module_output->set_translation_current(U_(InputStates::kId1));
+    }
+    {
+      auto module_output = output->add_back_left_output();
+      module_output->set_rotation_current(U_(InputStates::kIs2) *
+                                          drive_current_scalar);
+      module_output->set_translation_current(U_(InputStates::kId2));
+    }
+    {
+      auto module_output = output->add_back_right_output();
+      module_output->set_rotation_current(U_(InputStates::kIs3) *
+                                          drive_current_scalar);
+      module_output->set_translation_current(U_(InputStates::kId3));
+    }
 
     // Ignore the return value of Send
     output_builder->CheckOk(output_builder->Send());
+  } else {
+    U_.setZero();
+    desired_heading_.reset();
   }
 
   if (status_builder != nullptr) {
     StatusStatic *status = status_builder->get();
 
-    auto front_left_status = status->add_front_left_status();
-    PopulateSwerveModuleRotation(front_left_status,
-                                 &front_left_status_buffer.message());
+    if (current_state.has_value()) {
+      naive_estimator_.PopulateStatus(status->add_naive_estimator());
+    }
 
-    auto front_right_status = status->add_front_right_status();
-    PopulateSwerveModuleRotation(front_right_status,
-                                 &front_right_status_buffer.message());
+    if (controller_result.has_value()) {
+      auto controller_status = status->add_linear_controller();
+      CHECK(FromEigen(controller_result->debug.goal.cast<double>(),
+                      controller_status->add_goal_state()));
+      CHECK(FromEigen(controller_result->debug.U_ff.cast<double>(),
+                      controller_status->add_feedforwards_currents()));
+      CHECK(FromEigen(controller_result->debug.U_feedback.cast<double>(),
+                      controller_status->add_feedback_currents()));
+      CHECK(FromEigen(
+          controller_result->debug.feedback_contributions.cast<double>(),
+          controller_status->add_feedback_contributions()));
+      controller_status->set_sb02od_result(
+          controller_result->debug.sb02od_exit_code);
+    }
 
-    auto back_left_status = status->add_back_left_status();
-    PopulateSwerveModuleRotation(back_left_status,
-                                 &back_left_status_buffer.message());
-
-    auto back_right_status = status->add_back_right_status();
-    PopulateSwerveModuleRotation(back_right_status,
-                                 &back_right_status_buffer.message());
+    imu_zeroer_.PopulateStatus(status->add_imu_zeroer());
 
     // Ignore the return value of Send
     status_builder->CheckOk(status_builder->Send());
   }
+
+  const aos::monotonic_clock::time_point sends_done =
+      aos::monotonic_clock::now();
+
+  VLOG(1) << "Loop took "
+          << aos::time::DurationInSeconds(sends_done - profiling_start_time)
+          << " sec total. "
+          << aos::time::DurationInSeconds(estimation_done -
+                                          profiling_start_time)
+          << " sec in estimation "
+          << aos::time::DurationInSeconds(controller_done - estimation_done)
+          << " in controller "
+          << aos::time::DurationInSeconds(sends_done - controller_done)
+          << " sec cleaning up";
 }
 
 }  // namespace frc971::control_loops::swerve
--- frc971/control_loops/swerve/swerve_control_loops.h
+++ frc971/control_loops/swerve/swerve_control_loops.h
@@ -6,13 +6,20 @@
 #include "frc971/control_loops/profiled_subsystem_generated.h"
 #include "frc971/control_loops/profiled_subsystem_static.h"
 #include "frc971/control_loops/static_zeroing_single_dof_profiled_subsystem.h"
+#include "frc971/control_loops/swerve/inverse_kinematics.h"
+#include "frc971/control_loops/swerve/linear_velocity_controller.h"
+#include "frc971/control_loops/swerve/naive_estimator.h"
 #include "frc971/control_loops/swerve/swerve_drivetrain_can_position_generated.h"
 #include "frc971/control_loops/swerve/swerve_drivetrain_goal_generated.h"
 #include "frc971/control_loops/swerve/swerve_drivetrain_output_static.h"
 #include "frc971/control_loops/swerve/swerve_drivetrain_position_generated.h"
 #include "frc971/control_loops/swerve/swerve_drivetrain_status_static.h"
 #include "frc971/control_loops/swerve/swerve_zeroing_static.h"
+#include "frc971/control_loops/swerve/velocity_ekf.h"
+#include "frc971/queues/gyro_generated.h"
+#include "frc971/wpilib/imu_batch_generated.h"
 #include "frc971/zeroing/continuous_absolute_encoder.h"
+#include "frc971/zeroing/imu_zeroer.h"
 
 namespace frc971::control_loops::swerve {
 
@@ -29,16 +36,20 @@ class SwerveControlLoops
     : public ::frc971::controls::ControlLoop<Goal, Position, StatusStatic,
                                              OutputStatic> {
  public:
+  typedef LinearVelocityController::Scalar Scalar;
   using AbsoluteEncoderSubsystem =
       ::frc971::control_loops::StaticZeroingSingleDOFProfiledSubsystem<
           ::frc971::zeroing::ContinuousAbsoluteEncoderZeroingEstimator,
           ::frc971::control_loops::AbsoluteEncoderProfiledJointStatus>;
+  using States = LinearVelocityController::States;
 
   explicit SwerveControlLoops(
       ::aos::EventLoop *event_loop,
       const frc971::control_loops::
           StaticZeroingSingleDOFProfiledSubsystemCommonParams *rotation_params,
       const SwerveZeroing *zeroing_params,
+      const NaiveEstimator::Parameters &params,
+      const LinearVelocityController::ControllerWeights &lvc_weights,
       const ::std::string &name = "/swerve");
 
  protected:
@@ -46,7 +57,24 @@ class SwerveControlLoops
       const Goal *goal, const Position *position,
       aos::Sender<OutputStatic>::StaticBuilder *output_builder,
       aos::Sender<StatusStatic>::StaticBuilder *status_builder) override;
-  AbsoluteEncoderSubsystem front_left_, front_right_, back_left_, back_right_;
+  int iteration_counter_ = 0;
+  aos::Fetcher<CanPosition> can_position_fetcher_;
+  aos::Fetcher<frc971::sensors::GyroReading> gyro_fetcher_;
+  aos::Fetcher<frc971::IMUValuesBatch> imu_fetcher_;
+  std::optional<double> yaw_gyro_zero_;
+  zeroing::Averager<double, 200> yaw_gyro_zeroer_;
+  frc971::zeroing::ImuZeroer imu_zeroer_;
+  NaiveEstimator naive_estimator_;
+  LinearVelocityController velocity_controller_;
+  struct JoystickHeadingGoal {
+    Scalar heading;
+    aos::monotonic_clock::time_point last_time;
+  };
+  std::optional<JoystickHeadingGoal> desired_heading_;
+  InverseKinematics<Scalar> inverse_kinematics_;
+  VelocityEkf<Scalar> velocity_ekf_;
+  LinearVelocityController::Input U_ = LinearVelocityController::Input::Zero();
+  bool ekf_initialized_ = false;
 };
 
 }  // namespace frc971::control_loops::swerve
