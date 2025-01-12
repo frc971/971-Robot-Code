98c629713fbf55bce41270a3fd4a365c3cdc6f93
James (Peilun) Li <jamespeilunli@gmail.com>
2025-01-11 20:52:11 -0800

    Add elevator python and szsdps common param constants
    * elevator.py copied from y2019 and adjusted to make jsons in addition to ccs and hs
    * `elevator:StaticZeroingSingleDOFProfiledSubsystemCommonParams` (szsdps common params) added to constants.fbs and assigned values in common.json
    
    Change-Id: Id659157a0fb7752af158d054da173e286094635f
    Signed-off-by: James (Peilun) Li <jamespeilunli@gmail.com>
    Signed-off-by: wesley <wesleychung98@gmail.com>

---
+++ y2025/control_loops/python/elevator.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+from frc971.control_loops.python import control_loop
+from frc971.control_loops.python import linear_system
+import copy
+import numpy
+import sys
+import gflags
+import glog
+
+FLAGS = gflags.FLAGS
+
+try:
+    gflags.DEFINE_bool('plot', False, 'If true, plot the loop response.')
+except gflags.DuplicateFlagError:
+    pass
+
+first_stage_mass = 0.7957
+carriage_mass = 2.754
+
+kElevator = linear_system.LinearSystemParams(
+    name='Elevator',
+    motor=control_loop.KrakenFOC(),
+    # TODO: update these constants
+    G=(8.0 / 82.0),
+    radius=2.25 * 0.0254 / 2.0,
+    mass=first_stage_mass + carriage_mass,
+    q_pos=0.070,
+    q_vel=1.35,
+    kalman_q_pos=0.12,
+    kalman_q_vel=2.00,
+    kalman_q_voltage=35.0,
+    kalman_r_position=0.05)
+
+
+def main(argv):
+    if FLAGS.plot:
+        R = numpy.matrix([[1.5], [0.0]])
+        linear_system.PlotKick(kElevator, R)
+        linear_system.PlotMotion(kElevator,
+                                 R,
+                                 max_velocity=2.0,
+                                 max_acceleration=15.0)
+
+    # Write the generated constants out to a file.
+    if len(argv) != 7:
+        glog.fatal(
+            'Expected .h file name and .cc file name for the elevator and integral elevator.'
+        )
+    else:
+        namespaces = ['y2025', 'control_loops', 'superstructure', 'elevator']
+        linear_system.WriteLinearSystem(kElevator, argv[1:4], argv[4:7],
+                                        namespaces)
+
+
+if __name__ == '__main__':
+    argv = FLAGS(sys.argv)
+    glog.init()
+    sys.exit(main(argv))
