bcf9ea9496b9cd912185b5d873c6b23903ab1f07
Maxwell Henderson <mxwhenderson@gmail.com>
2025-01-14 22:20:40 -0800

    Fix swerve joystick input not being included in y2025
    This commit is fixing an issue we have on main, should've checked before
    merging.
    
    Change-Id: Ib32d6e21d42c364af89e1fc15e1640d94bc33ebd
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>

--- y2025/joystick_reader.cc
+++ y2025/joystick_reader.cc
@@ -44,9 +44,15 @@ namespace swerve = frc971::control_loops::swerve;
 // ButtonLocation constants go here
 class Reader : public ::frc971::input::SwerveJoystickInput {
  public:
-  Reader(::aos::EventLoop *event_loop, const y2025::Constants *robot_constants)
-      : ::frc971::input::SwerveJoystickInput(event_loop,
-                                             {.use_redundant_joysticks = true}),
+  Reader(::aos::EventLoop *event_loop,
+         const y2025::RobotConstants *robot_constants)
+      : ::frc971::input::SwerveJoystickInput(
+            event_loop,
+            {.vx_offset = robot_constants->input_config()->vx_offset(),
+             .vy_offset = robot_constants->input_config()->vy_offset(),
+             .omega_offset = robot_constants->input_config()->omega_offset(),
+             .use_redundant_joysticks =
+                 robot_constants->input_config()->use_redundant_joysticks()}),
         superstructure_goal_sender_(
             event_loop->MakeSender<GoalStatic>("/superstructure")),
         superstructure_status_fetcher_(
@@ -73,7 +79,7 @@ class Reader : public ::frc971::input::SwerveJoystickInput {
  private:
   ::aos::Sender<GoalStatic> superstructure_goal_sender_;
   ::aos::Fetcher<Status> superstructure_status_fetcher_;
-  const y2025::Constants *robot_constants_;
+  const y2025::RobotConstants *robot_constants_;
 };
 
 }  // namespace y2025::input::joysticks
@@ -88,10 +94,10 @@ int main(int argc, char **argv) {
   ::aos::ShmEventLoop constant_fetcher_event_loop(&config.message());
   frc971::constants::ConstantsFetcher<y2025::Constants> constants_fetcher(
       &constant_fetcher_event_loop);
-  const y2025::Constants *robot_constants = &constants_fetcher.constants();
+  const y2025::RobotConstants *robot_constants =
+      constants_fetcher.constants().robot();
 
   ::aos::ShmEventLoop event_loop(&config.message());
-  (void)robot_constants;
   ::y2025::input::joysticks::Reader reader(&event_loop, robot_constants);
 
   event_loop.Run();
