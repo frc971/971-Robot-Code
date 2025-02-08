d603d2e8e455caf694aa0e8a85ad20f0fa27cb1b
Maxwell Henderson <mxwhenderson@gmail.com>
2025-02-07 17:07:29 -0800

    Add 2025 target map and use Euler Angles
    We previously used quarternions which are confusing to work with
    epsecially from CAD. A euler angle to quaternion is added when fecthing
    the matrix.
    
    Change-Id: I5192c4603ad30a9418a4182d6c2e970e5288e8cb
    Signed-off-by: Justin Turcotte <jjturcot@gmail.com>

--- frc971/vision/target_map_utils.cc
+++ frc971/vision/target_map_utils.cc
@@ -1,5 +1,7 @@
 #include "frc971/vision/target_map_utils.h"
 
+#include <numbers>
+
 namespace frc971::vision {
 Eigen::Matrix<double, 4, 4> PoseToTransform(
     const frc971::vision::TargetPoseFbs *pose) {
@@ -9,6 +11,28 @@ Eigen::Matrix<double, 4, 4> PoseToTransform(
               Eigen::Vector3d(position->x(), position->y(), position->z())) *
           Eigen::Quaterniond(quaternion->w(), quaternion->x(), quaternion->y(),
                              quaternion->z())
+              .normalized())
+      .matrix();
+}
+
+template <typename Scalar>
+Scalar degToRad(Scalar degrees) {
+  return degrees * std::numbers::pi / 180.0;
+}
+
+Eigen::Matrix<double, 4, 4> PoseToTransform(
+    const frc971::vision::TargetPoseFieldFbs *pose) {
+  const frc971::vision::PositionField *position = pose->position();
+  const frc971::vision::EulerAngle *euler_angle = pose->orientation();
+  return (Eigen::Translation3d(
+              Eigen::Vector3d(position->x(), position->y(), position->z())) *
+          Eigen::Quaterniond(
+              Eigen::AngleAxisd(degToRad(euler_angle->x() - 90.0),
+                                Eigen::Vector3d::UnitX()) *
+              Eigen::AngleAxisd(degToRad(euler_angle->y()),
+                                Eigen::Vector3d::UnitY()) *
+              Eigen::AngleAxisd(degToRad(euler_angle->z()),
+                                Eigen::Vector3d::UnitZ()))
               .normalized())
       .matrix();
 }
--- frc971/vision/target_map_utils.h
+++ frc971/vision/target_map_utils.h
@@ -3,12 +3,16 @@
 
 #include <Eigen/Dense>
 
+#include "frc971/vision/target_map_field_generated.h"
 #include "frc971/vision/target_map_generated.h"
 
 namespace frc971::vision {
 // Converts a TargetPoseFbs into a transformation matrix.
 Eigen::Matrix<double, 4, 4> PoseToTransform(
     const frc971::vision::TargetPoseFbs *pose);
+Eigen::Matrix<double, 4, 4> PoseToTransform(
+    const frc971::vision::TargetPoseFieldFbs *pose);
+
 }  // namespace frc971::vision
 
 #endif  // FRC971_VISION_TARGET_MAP_UTILS_H_
