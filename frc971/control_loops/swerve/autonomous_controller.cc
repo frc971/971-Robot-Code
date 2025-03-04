96fc4b0c1e9472e92dfc4c6e1e25958a1d543f22
Justin Turcotte <jjturcot@gmail.com>
2025-03-04 09:14:14 -0800

    Do more tuning on auto controller
    Change-Id: Iacfbc1c0ee48b519b0009785ad7d79bae5fc0773
    Signed-off-by: Maxwell Henderson <mxwhenderson@gmail.com>
    Signed-off-by: Justin Turcotte <jjturcot@gmail.com>

--- frc971/control_loops/swerve/autonomous_controller.cc
+++ frc971/control_loops/swerve/autonomous_controller.cc
@@ -7,10 +7,17 @@
 #include "frc971/input/robot_state_generated.h"
 #include "frc971/math/flatbuffers_matrix.h"
 
-ABSL_FLAG(double, kPositionGain, 4.0, "Proportional gain for positional error");
-ABSL_FLAG(double, kRotationGain, 4.0, "Proportional gain for rotational error");
-ABSL_FLAG(double, kVelocityGain, 0.0, "Proportional gain for velocity error");
-ABSL_FLAG(double, kOmegaGain, 0.0,
+ABSL_FLAG(double, kPositionGain, 6.0, "Proportional gain for positional error");
+ABSL_FLAG(double, kRotationGain, 6.0, "Proportional gain for rotational error");
+ABSL_FLAG(double, kNextPositionGain, 1.0,
+          "Proportional gain for positional error");
+ABSL_FLAG(double, kNextRotationGain, 0.0,
+          "Proportional gain for rotational error");
+ABSL_FLAG(double, kVelocityGain, 0.1, "Proportional gain for velocity error");
+ABSL_FLAG(double, kNextVelocityGain, 0.5,
+          "Proportional gain for velocity error");
+ABSL_FLAG(double, kNextOmegaGain, 0.0, "Proportional gain for velocity error");
+ABSL_FLAG(double, kOmegaGain, 1.0,
           "Proportional gain for angular velocity error");
 
 using frc971::control_loops::swerve::AutonomousController;
@@ -138,6 +145,12 @@ void AutonomousController::Iterate() {
   auto builder = swerve_goal_sender_.MakeStaticBuilder();
   auto trajectory_point = trajectory_.message().discretized_trajectory()->Get(
       trajectory_index_.value());
+  auto next_trajectory_point =
+      (trajectory_index_.value() + 5 ==
+       trajectory_.message().discretized_trajectory()->size())
+          ? trajectory_point
+          : trajectory_.message().discretized_trajectory()->Get(
+                trajectory_index_.value() + 5);
 
   localizer_state_fetcher_.Fetch();
 
@@ -152,7 +165,7 @@ void AutonomousController::Iterate() {
   }
 
   constexpr double dt =
-      aos::time::DurationInSeconds(std::chrono::milliseconds(5));
+      aos::time::DurationInSeconds(std::chrono::milliseconds(2));
   const double vx = localizer_state_fetcher_->has_vx()
                         ? localizer_state_fetcher_->vx()
                         : (x - prev_x_) / dt;
@@ -176,25 +189,33 @@ void AutonomousController::Iterate() {
   const double goal_vy = flip_mult * trajectory_point->velocity()->y();
   const double goal_omega = trajectory_point->velocity()->theta();
 
+  const double next_goal_x = flip_mult * next_trajectory_point->position()->x();
+  const double next_goal_y = flip_mult * next_trajectory_point->position()->y();
+  const double next_goal_theta =
+      flip_mult * next_trajectory_point->position()->theta();
+
+  const double next_goal_vx =
+      flip_mult * next_trajectory_point->velocity()->x();
+  const double next_goal_vy =
+      flip_mult * next_trajectory_point->velocity()->y();
+  const double next_goal_omega =
+      flip_mult * next_trajectory_point->velocity()->theta();
+
   double x_error = x - goal_x;
   double y_error = y - goal_y;
   double theta_error = aos::math::NormalizeAngle(theta - goal_theta);
 
-  const double vx_error = vx - goal_vx;
-  const double vy_error = vy - goal_vy;
-  const double omega_error = omega - goal_omega;
+  double vx_error = vx - goal_vx;
+  double vy_error = vy - goal_vy;
+  double omega_error = omega - goal_omega;
 
-  constexpr double kThreshold = 0.005;
+  double next_x_error = x - next_goal_x;
+  double next_y_error = y - next_goal_y;
+  double next_theta_error = theta - next_goal_theta;
 
-  if (std::abs(x_error) < kThreshold) {
-    x_error = 0.0;
-  }
-  if (std::abs(y_error) < kThreshold) {
-    y_error = 0.0;
-  }
-  if (std::abs(theta_error) < kThreshold) {
-    theta_error = 0.0;
-  }
+  double next_vx_error = vx - next_goal_vx;
+  double next_vy_error = vy - next_goal_vy;
+  double next_omega_error = omega - next_goal_omega;
 
   prev_x_ = x;
   prev_y_ = y;
@@ -208,20 +229,18 @@ void AutonomousController::Iterate() {
   VLOG(1) << "omega error: " << omega_error;
 
   double vx_commanded = goal_vx - x_error * absl::GetFlag(FLAGS_kPositionGain) -
-                        vx_error * absl::GetFlag(FLAGS_kVelocityGain);
+                        vx_error * absl::GetFlag(FLAGS_kVelocityGain) -
+                        next_vx_error * absl::GetFlag(FLAGS_kNextVelocityGain) -
+                        next_x_error * absl::GetFlag(FLAGS_kNextPositionGain);
   double vy_commanded = goal_vy - y_error * absl::GetFlag(FLAGS_kPositionGain) -
-                        vy_error * absl::GetFlag(FLAGS_kVelocityGain);
-  double omega_commanded = goal_omega -
-                           theta_error * absl::GetFlag(FLAGS_kRotationGain) -
-                           omega_error * absl::GetFlag(FLAGS_kOmegaGain);
-
-  auto debug_builder = autonomous_controller_debug_sender_.MakeStaticBuilder();
-  debug_builder->set_x_error(x_error);
-  debug_builder->set_y_error(y_error);
-  debug_builder->set_theta_error(theta_error);
-  debug_builder->set_vx_error(vx_error);
-  debug_builder->set_vy_error(vy_error);
-  debug_builder->set_omega_error(omega_error);
+                        vy_error * absl::GetFlag(FLAGS_kVelocityGain) -
+                        next_vy_error * absl::GetFlag(FLAGS_kNextVelocityGain) -
+                        next_y_error * absl::GetFlag(FLAGS_kNextPositionGain);
+  double omega_commanded =
+      goal_omega - theta_error * absl::GetFlag(FLAGS_kRotationGain) -
+      omega_error * absl::GetFlag(FLAGS_kOmegaGain) -
+      next_omega_error * absl::GetFlag(FLAGS_kNextOmegaGain) -
+      next_theta_error * absl::GetFlag(FLAGS_kNextRotationGain);
 
   auto joystick_goal = builder->add_joystick_goal();
   joystick_goal->set_vx(vx_commanded);
@@ -239,6 +258,17 @@ void AutonomousController::Iterate() {
   }
 
   builder.CheckOk(builder.Send());
+
+  auto debug_builder = autonomous_controller_debug_sender_.MakeStaticBuilder();
+  debug_builder->set_x_error(x_error);
+  debug_builder->set_y_error(y_error);
+  debug_builder->set_theta_error(theta_error);
+  debug_builder->set_vx_error(vx_error);
+  debug_builder->set_goal_vx(goal_vx);
+  debug_builder->set_vy_error(vy_error);
+  debug_builder->set_goal_vy(goal_vy);
+  debug_builder->set_omega_error(omega_error);
+  debug_builder->set_goal_omega(goal_omega);
   debug_builder.CheckOk(debug_builder.Send());
 };
 
