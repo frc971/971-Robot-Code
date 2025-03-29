fa6b9017468873f6016dbb952b7fe3d06ace425d
Charlie Huang <charliehuang09@gmail.com>
2025-03-28 21:25:59 -0700

    Add enough space check for framer logger
    Change-Id: I0917a75dd8444fd0b25222f6938ee22ed12cfb7f
    Signed-off-by: Charlie Huang <charliehuang09@gmail.com>

--- y2025/vision/frame_logger.cc
+++ y2025/vision/frame_logger.cc
@@ -7,22 +7,54 @@
 
 ABSL_FLAG(bool, camera_zero, true, "If we are logging on camera zero");
 ABSL_FLAG(bool, camera_one, true, "If we are logging on camera one");
+ABSL_FLAG(unsigned long long int, space_threshold,
+          50ull * 1024ull * 1024ull * 1024ull,
+          "At least this amount of space left");
 
 namespace y2025::vision {
 
 using aos::monotonic_clock;
 
-FrameLogger::FrameLogger(aos::EventLoop *event_loop) {
+FrameLogger::FrameLogger(aos::EventLoop *event_loop)
+    : filesystem_status_(
+          event_loop->MakeFetcher<aos::util::FilesystemStatus>("/aos")),
+      index_(0) {
   auto logImage = [this, event_loop](std::string path) {
     return [this, event_loop, path](const CameraImage &image) {
+      // Log equal amounts of each image
+      if (last_logged_ == path && absl::GetFlag(FLAGS_camera_zero) &&
+          absl::GetFlag(FLAGS_camera_one)) {
+        return;
+      }
+
+      filesystem_status_.Fetch();
+      bool enough_space = true;
+      if (filesystem_status_.get() != nullptr) {
+        enough_space = false;
+        for (const aos::util::Filesystem *fs :
+             *filesystem_status_->filesystems()) {
+          CHECK(fs->has_path());
+          if (fs->path()->string_view() == "/" &&
+              fs->free_space() > absl::GetFlag(FLAGS_space_threshold)) {
+            enough_space = true;
+          }
+        }
+      }
+      if (!enough_space) {
+        return;
+      }
+
       const monotonic_clock::time_point eof = monotonic_clock::time_point(
           std::chrono::nanoseconds(image.monotonic_timestamp_ns()));
       const monotonic_clock::duration age = event_loop->monotonic_now() - eof;
       if (age > std::chrono::milliseconds(100)) {
-        VLOG(1) << "Behind, skipping image on camera0";
+        VLOG(1) << "Behind, skipping image on " << path;
         return;
       }
+
       LogImage(path, &image);
+      this->last_logged_ = path;
+      this->index_ += 1;
     };
   };
   if (absl::GetFlag(FLAGS_camera_zero)) {
@@ -46,8 +78,7 @@ void FrameLogger::LogImage(const std::string channel_name,
   compression_params.push_back(0);
 
   const std::string filepath = "/home/pi/images/" + channel_name + "-" +
-                               std::to_string(image->monotonic_timestamp_ns()) +
-                               ".png";
+                               std::to_string(this->index_) + ".png";
 
   cv::imwrite(filepath, image_mat, compression_params);
   VLOG(0) << "Added Image: " << filepath;
--- y2025/vision/frame_logger.h
+++ y2025/vision/frame_logger.h
@@ -1,9 +1,12 @@
 #ifndef Y2025_FRAME_LOGGER_H_
 #define Y2025_FRAME_LOGGER_H_
 
+#include <string>
+
 #include <opencv2/opencv.hpp>
 
 #include "aos/events/event_loop.h"
+#include "aos/util/filesystem_generated.h"
 #include "frc971/vision/vision_generated.h"
 
 namespace y2025::vision {
@@ -20,6 +23,9 @@ class FrameLogger {
   aos::Fetcher<frc971::vision::CameraImage> camera_zero_fetcher_;
   aos::Fetcher<frc971::vision::CameraImage> camera_one_fetcher_;
   std::unique_ptr<aos::PhasedLoopHandler> phased_loop_handle_;
+  aos::Fetcher<aos::util::FilesystemStatus> filesystem_status_;
+  std::string last_logged_ = "";
+  int index_;
 };
 
 }  // namespace y2025::vision
